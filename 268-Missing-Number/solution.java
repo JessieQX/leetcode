1255ca1b0493b5c6bb4793cd686e2fac8596070f
JessieQX <quxuan819@gmail.com>
2016-05-16 17:54:37 -0400

    [268. Missing Number][Accepted]committed by LeetCode Extension

--- 268-Missing-Number/solution.java
+++ 268-Missing-Number/solution.java
@@ -1,20 +1,10 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        if (nums[0] != 0 && nums.length > 1 || nums.length == 1 && nums[0] == 1) {
-            return 0;
-        } 
-        int count = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] - nums[i - 1] == 1) {
-                count++;
-            } else {
-                break;
-            }
+        int len = nums.length;
+        int sum = (0 + len) * (len + 1) / 2;
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
         }
-        //return count + 2;
-        
-        return nums[count] + 1;
-        
+        return sum;
     }
 }
