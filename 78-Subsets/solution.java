71c6ef9a8970c09dd0302c033cf22ac3a7d0eaeb
JessieQX <quxuan819@gmail.com>
2016-05-16 21:13:55 -0400

    [78. Subsets][Compile Error]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,17 +1,31 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-    Arrays.sort(nums);
-    int totalNumber = 1 << nums.length;
-    List<List<Integer>> collection = new ArrayList<List<Integer>>(totalNumber);
-    for (int i=0; i<totalNumber; i++) {
-        List<Integer> set = new LinkedList<Integer>();
-        for (int j=0; j<nums.length; j++) {
-            if ((i & (1<<j)) != 0) {
-                set.add(nums[j]);
+    // Arrays.sort(nums);
+    // int totalNumber = 1 << nums.length;
+    // List<List<Integer>> collection = new ArrayList<List<Integer>>(totalNumber);
+    // for (int i=0; i<totalNumber; i++) {
+    //     List<Integer> set = new LinkedList<Integer>();
+    //     for (int j=0; j<nums.length; j++) {
+    //         if ((i & (1<<j)) != 0) {
+    //             set.add(nums[j]);
+    //         }
+    //     }
+    //     collection.add(set);
+    // }
+    // return collection;
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<Integer>());
+
+        Arrays.sort(S);
+        for(int i : S) {
+            List<List<Integer>> tmp = new ArrayList<>();
+            for(List<Integer> sub : res) {
+                List<Integer> a = new ArrayList<>(sub);
+                a.add(i);
+                tmp.add(a);
             }
+            res.addAll(tmp);
         }
-        collection.add(set);
-    }
-    return collection;
+        return res;
     }
 }
