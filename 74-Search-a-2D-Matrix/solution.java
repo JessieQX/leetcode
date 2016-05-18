fcfc3f16a829a1ecba7abae67342c3c4f030aa25
JessieQX <quxuan819@gmail.com>
2016-05-18 15:20:48 -0400

    [74. Search a 2D Matrix][Accepted]committed by LeetCode Extension

--- 74-Search-a-2D-Matrix/solution.java
+++ 74-Search-a-2D-Matrix/solution.java
@@ -23,7 +23,8 @@ public class Solution {
             return false;
         }
         
-        start = 0, end = col - 1;
+        start = 0; 
+        end = col - 1;
         while (start + 1 < end) {
             int mid = start + (end - start) / 2;
             if (matrix[row][mid] == target) {
