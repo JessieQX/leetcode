3ee160431f6c29d7d926eca8d7418d6812f97cb7
JessieQX <quxuan819@gmail.com>
2016-05-18 15:05:58 -0400

    [74. Search a 2D Matrix][Time Limit Exceeded]committed by LeetCode Extension

--- 74-Search-a-2D-Matrix/solution.java
+++ 74-Search-a-2D-Matrix/solution.java
@@ -2,32 +2,39 @@ public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
-        for (int i = 0; i < row; i++) {
-            //int start = matrix[i][0], end = matrix[i][col - 1];
-            int start = 0, end = col - 1;
-            System.out.println("start: " + start);
-            System.out.println("end: " + end);
-            //for (int j = 0; j < col; j++) {
-                while (start < end) {
-                    int mid = start + (end - start) / 2;
-                    System.out.println(mid);
-                    if (matrix[i][mid] == target) {
-                        return true;
-                    }
-                    if (matrix[i][mid] < target) {
-                        start = mid + 1;
-                    } else {
-                        end = mid;
-                    }
-                }
-                if (matrix[i][start] == target) {
-                    return true;
-                }
-                if (matrix[i][end] == target) {
-                    return true;
-                }
-            //}
+        int targetRow = searchRow(matrix, target);
+        int start = 0, end = col - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[targetRow][mid] == target) {
+                return true;
+            }
+            if (matrix[targetRow][mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        if (matrix[targetRow][start] == target || matrix[targetRow][end] == target) {
+            return true;
         }
         return false;
+    }
+    private int searchRow(int[][] matrix, int target) {
+        int row = matrix.length;
+        
+        int start = 0, end = row - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[mid][0] == target) {
+                return mid;
+            }
+            if (matrix[mid][0] < target) {
+                start = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start;
     }
 }
