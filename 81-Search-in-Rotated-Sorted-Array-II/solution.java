f7935a43954846b4bf08666ebe855462b60aaa5f
JessieQX <quxuan819@gmail.com>
2016-05-18 13:09:38 -0400

    [81. Search in Rotated Sorted Array II][Compile Error]committed by LeetCode Extension

---
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int midIdx = findMinIdx(nums);
+        int len = nums.length;
+        if (target == nums[minIdx]) {
+            return true;
+        }
+        int start = (target <= nums[len - 1]) ? minIdx : 0;
+        int end = (target > nums[len - 1]) ? minIdx : len - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        if (nums[start] == target || nums[end] == target) {
+            return true;
+        }
+        return false;
+    }
+    private int findMinIdx(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
