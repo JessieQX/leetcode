7bd72fd993d3306e544913e96ef5638c7b66f3ad
JessieQX <quxuan819@gmail.com>
2016-05-18 13:30:34 -0400

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -1,38 +1,33 @@
 public class Solution {
     public boolean search(int[] nums, int target) {
-        int midIdx = findMinIdx(nums);
-        int len = nums.length;
-        if (target == nums[midIdx]) {
-            return true;
-        }
-        int start = (target <= nums[len - 1]) ? minIdx : 0;
-        int end = (target > nums[len - 1]) ? minIdx : len - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
+        int start = 0, end = nums.length - 1, mid = -1;
+        while(start <= end) {
+            mid = (start + end) / 2;
             if (nums[mid] == target) {
                 return true;
             }
-            if (nums[mid] < target) {
-                start = mid + 1;
+            //If we know for sure right side is sorted or left side is unsorted
+            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            //If we know for sure left side is sorted or right side is unsorted
+            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
+            //any of the two sides won't change the result but can help remove duplicate from
+            //consideration, here we just use end-- but left++ works too
             } else {
-                end = mid;
+                end--;
             }
         }
-        if (nums[start] == target || nums[end] == target) {
-            return true;
-        }
+
         return false;
-    }
-    private int findMinIdx(int[] nums) {
-        int start = 0, end = nums.length - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[end]) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        return start;
     }
 }
