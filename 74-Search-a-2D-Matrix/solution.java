832a835bc31f6d05b6aae69c54ef630eafca3f7d
JessieQX <quxuan819@gmail.com>
2016-05-18 15:20:02 -0400

    [74. Search a 2D Matrix][Compile Error]committed by LeetCode Extension

--- 74-Search-a-2D-Matrix/solution.java
+++ 74-Search-a-2D-Matrix/solution.java
@@ -2,39 +2,45 @@ public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int row = matrix.length;
         int col = matrix[0].length;
-        int targetRow = searchRow(matrix, target);
-        int start = 0, end = col - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (matrix[targetRow][mid] == target) {
-                return true;
-            }
-            if (matrix[targetRow][mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        if (matrix[targetRow][start] == target || matrix[targetRow][end] == target) {
-            return true;
-        }
-        return false;
-    }
-    private int searchRow(int[][] matrix, int target) {
-        int row = matrix.length;
         
         int start = 0, end = row - 1;
-        while (start < end) {
+        while (start  + 1< end) {
             int mid = start + (end - start) / 2;
             if (matrix[mid][0] == target) {
-                return mid;
+                return true;
             }
             if (matrix[mid][0] < target) {
                 start = mid;
             } else {
-                end = mid - 1;
+                end = mid;
             }
         }
-        return start;
+        if (matrix[end][0] <= target) {
+            row = end;
+        } else if (matrix[start][0] <= target) {
+            row = start;
+        } else {
+            return false;
+        }
+        
+        int start = 0, end = col - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+            if (matrix[row][mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        if (matrix[row][start] == target) {
+            return true;
+        } else if (matrix[row][end] == target) {
+            return true;
+        }
+        return false;
     }
+    
 }
