9a7cff533ab918580bea7a31c513da8d79a0bf6e
JessieQX <quxuan819@gmail.com>
2016-05-18 20:26:59 -0400

    [347. Top K Frequent Elements][Compile Error]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -1,30 +1,21 @@
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        Arrays.sort(nums);
-        List<Integer> list = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        int count = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i + 1] == nums[i]) {
-                count++;
-            } else {
-                map.put(nums[i], count);
-            }
-            
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int num: nums){
+            map.put(num, map.containsKey(num)? map.get(num) + 1 : 1);
         }
-        if (!map.containsKey(nums[nums.length - 1])) {
-            map.put(nums[nums.length - 1], 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = 
+        new PriorityQueue<Map.Entry<Integer, Integer>>(map.size(), (o1, o2) -> o2.getValue() - o1.getValue());
+        queue.addAll(map.entrySet());
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < k; i++){
+            res.add(queue.poll().getKey());
         }
-        int[] a = new int[map.size()];
-        for (int i = 0; i < map.size(); i++) {
-            for (int key : map.keySet()) {
-                a[i] = map.get(key);
-            }
-        }
-        Arrays.sort(a);
-        for (int i = 0; i < k; i++) {
-            list.add(a[i]);
-        }
-        return list;
+        return res;
+    }
+    public static void main(String[] args) {
+        TopK topK = new TopK();
+        int[] nums = {1,1,2,3,4,3,2,2};
+        System.out.println(topK.topKFrequent(nums, 3));
     }
 }
