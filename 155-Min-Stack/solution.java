a458f0fbdccc8e547ffff2294cbbce946782c3af
JessieQX <quxuan819@gmail.com>
2016-05-18 21:01:02 -0400

    [155. Min Stack][Compile Error]committed by LeetCode Extension

---
+++ 155-Min-Stack/solution.java
@@ -0,0 +1,42 @@
+public class MinStack {
+    private Stack stack1;
+    private Stack stack2;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack1 = new Stack();
+        stack2 = new Stack();
+    }
+    
+    public void push(int x) {
+        stack1.push(x);
+        if (stack2 == null) {
+            stack2.push(x);
+        } else if (stack2!=null) {
+            if (x < stack2.peek()) {
+                stack2.pop();
+                stack2.push(x);
+            } 
+        }
+    }
+    
+    public void pop() {
+        stack1.pop();
+    }
+    
+    public int top() {
+        return stack1.peek();
+    }
+    
+    public int getMin() {
+        return stack2.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
