4d879e6c52b76b1a3adf83028862e3ecab4fafed
JessieQX <quxuan819@gmail.com>
2016-05-26 17:32:14 -0400

    [151. Reverse Words in a String][Compile Error]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -3,20 +3,33 @@ public class Solution {
         if (s == null || s.length() == 0) {
             return s;
         }
-        Stack<String> stack = new Stack<>();
+        // Stack<String> stack = new Stack<>();
+        // String[] word = s.trim().split(" ");
+        // int len = word.length;
+        // for (String ss : word) {
+        //     if (ss.length() == 0) {
+        //         len--;
+        //     } else {
+        //         stack.push(ss);
+        //     }
+        // }
+        // String a = new String();
+        // for (int i = 0; i < len; i++) {
+        //     a = a + stack.pop() + " ";
+        // }
+        // return a.trim();
         String[] word = s.trim().split(" ");
-        int len = word.length;
-        for (String ss : word) {
-            //ss = ss.trim();
-            if (ss.length() == 0) {
-                len--;
-            } else {
-                stack.push(ss);
-            }
+        int i = 0, j = word.length - 1;
+        while (i < j) {
+            int temp = word[i];
+            word[i] = word[j];
+            word[j] = temp;
+            i++;
+            j--;
         }
         String a = new String();
-        for (int i = 0; i < len; i++) {
-            a = a + stack.pop() + " ";
+        for (int ss : word) {
+            a += ss + " ";
         }
         return a.trim();
     }
