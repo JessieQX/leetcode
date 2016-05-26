e6ba4f1237453b90c547d828092a225cde339bb1
JessieQX <quxuan819@gmail.com>
2016-05-26 17:40:47 -0400

    [151. Reverse Words in a String][Accepted]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -18,27 +18,33 @@ public class Solution {
         //     a = a + stack.pop() + " ";
         // }
         // return a.trim();
-        String[] word = s.trim().split(" ");
-        int i = 0, j = word.length - 1;
-        while (i < j) {
-            if (word[i].length() == 0) {
-                i++;
-            }
-            if (word[j].length() == 0) {
-                j--;
-            }
-            String temp = word[i];
-            word[i] = word[j];
-            word[j] = temp;
-            i++;
-            j--;
+        // String[] word = s.trim().split(" ");
+        // int i = 0, j = word.length - 1;
+        // while (i < j) {
+        //     if (word[i].length() == 0) {
+        //         i++;
+        //     }
+        //     if (word[j].length() == 0) {
+        //         j--;
+        //     }
+        //     String temp = word[i];
+        //     word[i] = word[j];
+        //     word[j] = temp;
+        //     i++;
+        //     j--;
+        // }
+        // String a = new String();
+        // for (String ss : word) {
+        //     if (ss.length() != 0) {
+        //         a += ss + " ";
+        //     }
+        // }
+        // return a.trim();
+        String[] word = s.trim().split("\\s+");
+        String res = "";
+        for (int i = word.length - 1; i > 0; i--) {
+            res += word[i] + " ";
         }
-        String a = new String();
-        for (String ss : word) {
-            if (ss.length() != 0) {
-                a += ss + " ";
-            }
-        }
-        return a.trim();
+        return res + word[0];
     }
 }
