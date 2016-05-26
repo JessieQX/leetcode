4fa04ee479c1a3da330854a018092c144e3b2aaa
JessieQX <quxuan819@gmail.com>
2016-05-26 13:41:39 -0400

    [118. Pascal's Triangle][Accepted]committed by LeetCode Extension

---
+++ 118-Pascal's-Triangle/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> allRows = new ArrayList<List<Integer>>();
+        if (numRows <= 0) {
+            return allRows;
+        }
+        List<Integer> row = new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+            allRows.add(new ArrayList<Integer>(row));
+        }
+        return allRows;
+    }
+}
