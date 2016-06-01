75901ae915f52c21a4f55288d787e5646c13d944
JessieQX <quxuan819@gmail.com>
2016-06-01 18:27:37 -0400

    [97. Interleaving String][Accepted]committed by LeetCode Extension

--- 97-Interleaving-String/solution.java
+++ 97-Interleaving-String/solution.java
@@ -6,18 +6,19 @@ public class Solution {
         int m = s1.length();
         int n = s2.length();
         boolean[][] f = new boolean[m + 1][n + 1];
-        for (int i = 0; i < m + 1; i++) {
+        f[0][0] = true;
+        for (int i = 1; i < m + 1; i++) {
             if (f[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1)) {
                 f[i][0] = true;
             }
         }
-        for (int j = 0; j < n + 1; j++) {
+        for (int j = 1; j < n + 1; j++) {
             if (f[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1)) {
                 f[0][j] = true;
             }
         }
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
                 if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && f[i - 1][j] ||
                 s2.charAt(j - 1) == s3.charAt(i + j - 1) && f[i][j - 1]) {
                     f[i][j] = true;
