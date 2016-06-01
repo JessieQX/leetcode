b79ff60039589b03bb7047143e27ace223f42c78
JessieQX <quxuan819@gmail.com>
2016-06-01 17:45:51 -0400

    [72. Edit Distance][Accepted]committed by LeetCode Extension

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -17,7 +17,7 @@ public class Solution {
         }
         for (int i = 1; i < len1 + 1; i++) {
             for (int j = 1; j < len2 + 1; j++) {
-                if (word1.charAt(i) == word2.charAt(j)) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     f[i][j] = Math.min(Math.min(f[i - 1][j] + 1, f[i][j - 1] + 1), f[i - 1][j - 1]);
                 } else {
                     f[i][j] = Math.min(Math.min(f[i - 1][j] + 1, f[i][j - 1] + 1), f[i - 1][j - 1] + 1);
