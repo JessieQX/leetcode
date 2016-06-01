6652cca6cdd42269d82df8b585c54fdf4adbc73d
JessieQX <quxuan819@gmail.com>
2016-06-01 17:43:45 -0400

    [72. Edit Distance][Runtime Error]committed by LeetCode Extension

---
+++ 72-Edit-Distance/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        if (word1 == null || word1.length() == 0) {
+            return word2.length();
+        }
+        if (word2 == null || word2.length() == 0) {
+            return word1.length();
+        }
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] f = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i < len1 + 1; i++) {
+            f[i][0] = i;
+        }
+        for (int j = 0; j < len2 + 1; j++) {
+            f[0][j] = j;
+        }
+        for (int i = 0; i < len1 + 1; i++) {
+            for (int j = 0; j < len2 + 1; j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    f[i][j] = Math.min(Math.min(f[i - 1][j] + 1, f[i][j - 1] + 1), f[i - 1][j - 1]);
+                } else {
+                    f[i][j] = Math.min(Math.min(f[i - 1][j] + 1, f[i][j - 1] + 1), f[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return f[len1][len2];
+    }
+}
