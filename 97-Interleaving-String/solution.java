e434c06509070ca132c68e93745914bcfe51d18f
JessieQX <quxuan819@gmail.com>
2016-06-01 18:26:52 -0400

    [97. Interleaving String][Runtime Error]committed by LeetCode Extension

---
+++ 97-Interleaving-String/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        int m = s1.length();
+        int n = s2.length();
+        boolean[][] f = new boolean[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++) {
+            if (f[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1)) {
+                f[i][0] = true;
+            }
+        }
+        for (int j = 0; j < n + 1; j++) {
+            if (f[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1)) {
+                f[0][j] = true;
+            }
+        }
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (s1.charAt(i - 1) == s3.charAt(i + j - 1) && f[i - 1][j] ||
+                s2.charAt(j - 1) == s3.charAt(i + j - 1) && f[i][j - 1]) {
+                    f[i][j] = true;
+                } 
+            }
+        }
+        return f[m][n];
+    }
+}
