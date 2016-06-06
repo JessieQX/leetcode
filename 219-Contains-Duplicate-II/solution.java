214cb6cacba4b8ba5398cf30fc6859473c866592
JessieQX <quxuan819@gmail.com>
2016-06-06 13:56:51 -0400

    [219. Contains Duplicate II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 219-Contains-Duplicate-II/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        // Set<Integer> set = new HashSet<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i > k)  set.remove(nums[i - k - 1]);//说明nums[i]在k的范围内没有相同的数，所以去掉nums[i]。
+        //     if (!set.add(nums[i])) return true;
+        // }
+        // return false;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			if (!map.containsKey(nums[i])) {
+				map.put(nums[i], i);
+			} else {
+				if (Math.abs(map.get(nums[i]) - i) <= k) {
+					return true;
+				}
+			}
+		}
+		return false;
+    }
+}
