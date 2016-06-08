0dd8f2b24c0faf964ac7fe51253fa8c75b86131f
JessieQX <quxuan819@gmail.com>
2016-06-07 21:29:08 -0400

    [216. Combination Sum III][Accepted]committed by LeetCode Extension

---
+++ 216-Combination-Sum-III/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        helper(result, curr, k, n, 1);
+        return result;
+    }
+    private void helper(List<List<Integer>> result, List<Integer> curr, int k, int target, int start) {
+        //int[] candidates = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //int count = 0;
+        if (target > 0 && curr.size() < k) {
+            for (int i = start; i <= 9; i++) {
+                //if (i > start && candidates[i] == candidates[i - 1]) continue;
+                curr.add(i);
+                helper(result, curr, k, target - i, i + 1);
+                curr.remove(curr.size() - 1);
+            }
+        } else if (target == 0 && curr.size() == k) {
+            result.add(new ArrayList<Integer>(curr));
+        }
+    }
+}
