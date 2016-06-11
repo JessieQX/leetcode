14df8c4b8d48b10ede84d43c25c79b3c44623c73
JessieQX <quxuan819@gmail.com>
2016-06-11 12:45:35 -0400

    [318. Maximum Product of Word Lengths][Time Limit Exceeded]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -7,7 +7,7 @@ public class Solution {
         for (int i = 0; i < words.length - 1; i++) {
             for (int j = i + 1; j < words.length; j++) {
                 if (!shareCommonLetter(words[i], words[j])) {
-                    max = Math.max(max, words[i].length * words[j].length);
+                    max = Math.max(max, words[i].length() * words[j].length());
                 }
             }
         }
