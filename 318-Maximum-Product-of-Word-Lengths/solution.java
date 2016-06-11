89c27f1bb7e327317c4ab5a4dc08f9e42fb2c52d
JessieQX <quxuan819@gmail.com>
2016-06-11 13:48:20 -0400

    [318. Maximum Product of Word Lengths][Accepted]committed by LeetCode Extension

--- 318-Maximum-Product-of-Word-Lengths/solution.java
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -3,11 +3,13 @@ public class Solution {
         if (words == null || words.length == 0) {
             return 0;
         }
+        //longer words are at front.
         Arrays.sort(words, new Comparator<String>() {
             public int compare(String s1, String s2) {
                 return s2.length() - s1.length();
             }
         });
+        //newWords is to turn String into Integer so we can figure out whether they have same characters.
         int[] newWords = new int[words.length];
         for (int i = 0; i < newWords.length; i++) {
             for (Character c : words[i].toCharArray()) {
@@ -16,9 +18,14 @@ public class Solution {
         }
         int max = 0;
         for (int i = 0; i < newWords.length - 1; i++) {
+            
+            //if words[i].length is not long enough, then break.
             if (words[i].length() * words[i].length() <= max) break;
             for (int j = i + 1; j < newWords.length; j++) {
+                
+                //compare whether two Strings have same characters.
                 if ((newWords[i] & newWords[j]) == 0) {
+                    //still use words[i] to multiple the length.
                     max = Math.max(max, words[i].length() * words[j].length());
                 }
             }
