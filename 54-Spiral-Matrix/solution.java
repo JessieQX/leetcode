523f43f19d7533fc6edc1e65c2937fa8e4f5c338
JessieQX <quxuan819@gmail.com>
2016-06-13 17:01:38 -0400

    [54. Spiral Matrix][Runtime Error]committed by LeetCode Extension

---
+++ 54-Spiral-Matrix/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+        
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            //left to right
+            for (int i = colBegin; i <= colEnd; i++) {
+                result.add(matrix[rowBegin][i]);
+            }
+            rowBegin++;
+            
+            //up to down
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                result.add(matrix[i][rowEnd]);
+            }
+            colEnd--;
+            
+            //right to left
+            for (int i = colEnd; i >= colBegin; i--) {
+                result.add(matrix[rowEnd][i]);
+            }
+            rowEnd--;
+            
+            //down to up
+            for (int i = rowEnd; i>= rowBegin; i--) {
+                result.add(matrix[i][rowBegin]);
+            }
+            rowBegin++;
+        }
+        return result;
+    }
+    
+}
