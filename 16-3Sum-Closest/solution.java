a489f6a3ed0a1fcfee8c43b10cf6c0451670c2bb
JessieQX <quxuan819@gmail.com>
2016-06-14 10:24:44 -0400

    [16. 3Sum Closest][Runtime Error]committed by LeetCode Extension

--- 16-3Sum-Closest/solution.java
+++ 16-3Sum-Closest/solution.java
@@ -1,11 +1,13 @@
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        int result = 0;
+        int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length; i++) {
             int sum = target - nums[i];
             int m = nums[i + 1];
             int n = nums[nums.length - 1];
-            int min = Integer.MAX_VALUE;
-            int result = 0;
+            
+
             while (m < n) {
                 int x = sum - m - n;
                 min = Math.min(Math.abs(x), min);
