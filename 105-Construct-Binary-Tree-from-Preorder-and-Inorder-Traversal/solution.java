b1c531abf85e88177a40e15919981cc16e5d98fc
JessieQX <quxuan819@gmail.com>
2016-06-15 20:05:16 -0400

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Compile Error]committed by LeetCode Extension

---
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        TreeNode root = new TreeNode(preorder[0]);
+        //int len = inorder.length;
+        int rootIndex = 0;
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == preorder[0]) {
+                rootIndex = i;
+            }
+        }
+        root.left = build(root, inorder, 0, rootIndex - 1);
+        root.right = build(root, inorder, rootIndex + 1, preorder.length - 1);
+        return root;
+    }
+    public TreeNode build(TreeNode root, int[] inorder, int start, int end) {
+        if (start >= end) {
+            return null;
+        }
+        
+        int mid = start + (end - start) / 2;
+        return root.left = build(root, inorder, 0, mid - 1) && root.right = build(root, inorder, mid + 1, end);
+    }
+}
