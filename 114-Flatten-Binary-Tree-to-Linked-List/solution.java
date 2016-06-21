bf48baf15b6b044b7c664dd094c4b0bbb038a577
JessieQX <quxuan819@gmail.com>
2016-06-21 15:45:44 -0400

    [114. Flatten Binary Tree to Linked List][Accepted]committed by LeetCode Extension

--- 114-Flatten-Binary-Tree-to-Linked-List/solution.java
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution.java
@@ -13,6 +13,9 @@ public class Solution {
         if (root == null) {
             return;
         }
+        //reverse pre-order recursion. find the right-est node and make it point to null. 
+        //Then make the right-est node as prev, make the last left node as root pointing to prev.
+        //Then make the root as prev, and find the last root as new root.
         flatten(root.right);
         flatten(root.left);
         root.right = prev;
