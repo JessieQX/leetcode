b0c1cde84c003f1124598669976af44727a10961
JessieQX <quxuan819@gmail.com>
2016-06-21 17:20:20 -0400

    [106. Construct Binary Tree from Inorder and Postorder Traversal][Accepted]committed by LeetCode Extension

--- 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.java
+++ 106-Construct-Binary-Tree-from-Inorder-and-Postorder-Traversal/solution.java
@@ -10,21 +10,21 @@
 public class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int len = postorder.length;
-        return build(inorder, postorder, 0, len - 1, len - 1);
+        return build(inorder, postorder, len - 1, 0, len - 1);
     }
     public TreeNode build(int[] inorder, int[] postorder, int start, int end, int postStart) {
-        if (postStart < 0 || start > end) {
+        if (postStart < 0 || start < end) {
             return null;
         }
         TreeNode root = new TreeNode(postorder[postStart]);
-        int index = 0;
-        for (int i = start; i <= end; i++) {
+        int index = start;
+        for (int i = start; i >= end; i--) {
             if (inorder[i] == root.val) {
                 index = i;
             }
         }
-        root.left = build(inorder, postorder, start, index - 1, postStart - start + index - 1);
-        root.right = build(inorder, postorder, index + 1, end, postStart - 1);
+        root.right = build(inorder, postorder, start, index + 1, postStart - 1);
+        root.left = build(inorder, postorder, index - 1, end, postStart - start + index - 1);
         return root;
     }
 }
