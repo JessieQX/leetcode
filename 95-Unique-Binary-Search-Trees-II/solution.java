c74d866721534c126e2ef774d660aae769fcc3c0
JessieQX <quxuan819@gmail.com>
2016-06-21 14:17:21 -0400

    [95. Unique Binary Search Trees II][Compile Error]committed by LeetCode Extension

--- 95-Unique-Binary-Search-Trees-II/solution.java
+++ 95-Unique-Binary-Search-Trees-II/solution.java
@@ -8,37 +8,7 @@
  * }
  */
 public class Solution {
-    // public List<TreeNode> generateTrees(int n) {
-    //     return genTreeList(1,n);
-    // }
-    // private List<TreeNode> genTreeList(int start, int end) {
-    //     List<TreeNode> list = new ArrayList<>();
-    //     if (start > end) {
-    //         list.add(null);
-    //         return list;
-    //     }
-    //     if (start == end) {
-    //         list.add(new TreeNode(start));
-    //         return list;
-    //     }
-    //     List<TreeNode> left, right;
-    //     for (int i = start; i <= end; i++) {
-    //         left = genTreeList(start, i - 1);
-    //         right = genTreeList(i + 1, end);
-    //         for (TreeNode lnode : left) {
-    //             for (TreeNode rnode : right) {
-    //                 TreeNode root = new TreeNode(i);
-    //                 root.left = lnode;
-    //                 root.right = rnode;
-    //                 list.add(root);
-    //             }
-    //         }
-    //     }
-    //     return list;
-    // }
     public List<TreeNode> generateTrees(int n) {
-
-        //return genTrees(1,n);
         List<TreeNode> list = new ArrayList<>();
         list = genTrees(1, n);
         if (list.get(0) == null) {
@@ -46,45 +16,31 @@ public class Solution {
         } else {
             return list;
         }
-
     }
-
-    public List<TreeNode> genTrees (int start, int end)
-    {
-
-        List<TreeNode> list = new ArrayList<TreeNode>();
-
-        if(start>end)
-        {
+    private List<TreeNode> genTreeList(int start, int end) {
+        List<TreeNode> list = new ArrayList<>();
+        if (start > end) {
             list.add(null);
             return list;
         }
-
-        if(start == end){
+        if (start == end) {
             list.add(new TreeNode(start));
             return list;
         }
-
-        List<TreeNode> left,right;
-        for(int i=start;i<=end;i++)
-        {
-
-            left = genTrees(start, i-1);
-            right = genTrees(i+1,end);
-
-            for(TreeNode lnode: left)
-            {
-                for(TreeNode rnode: right)
-                {
+        List<TreeNode> left, right;
+        for (int i = start; i <= end; i++) {
+            left = genTreeList(start, i - 1);
+            right = genTreeList(i + 1, end);
+            for (TreeNode lnode : left) {
+                for (TreeNode rnode : right) {
                     TreeNode root = new TreeNode(i);
                     root.left = lnode;
                     root.right = rnode;
                     list.add(root);
                 }
             }
-
         }
-
         return list;
     }
+   
 }
