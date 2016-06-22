877e711b397ed9e639adeeacc0a16f0662b00da0
JessieQX <quxuan819@gmail.com>
2016-06-22 18:04:26 -0400

    [146. LRU Cache][Compile Error]committed by LeetCode Extension

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -1,23 +1,76 @@
 public class LRUCache {
+    class DLinkedList {
+        int key;
+        int value;
+        DLinkedList pre;
+        DLinkedList post;
+    }
+    /*
+     * add node just after head
+     */
+    private void addNode(DLinkedList node) {
+        node.pre = head;
+        node.post = head.post;
+        head.post.pre = node;
+        head.post = node;
+    }
+    private void removeNode(DLinedList node) {
+        DLinkedList preNode = node.pre;
+        DLinkedList postNode = node.post;
+        preNode.post = postNode;
+        postNode.pre = preNode;
+    }
+    private void moveToHead(DLinkedList node) {
+        this.removeNode(node);
+        this.addNode(node);
+    }
+    private DLinkedList popTail() {
+        DLinkedList node = tail.pre;
+        this.removeNode(node);
+        return node;
+    }
+    
+    private HashTable<Integer, DLinkedList> cache = new HashTable<>();
     private int capacity;
-    Map<Integer, Integer> map = new HashMap<>();
+    private int count;
+    private DLinkedList head, tail;
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        this.count = 0;
+        head = new DLinkedList();
+        head = null;
+        tail = new DLinkedList();
+        tail = null;
+        head.post = tail;
+        tail.pre = head;
     }
     
     public int get(int key) {
-        if (map.containsKey()) {
-            return map.get(key);
-        } else {
+        DLinkedList node = cache.get(key);
+        if (node == null) {
             return -1;
         }
+        this.moveToHead(node);
+        return node.value;
     }
     
     public void set(int key, int value) {
-        if (map.size() < capacity) {
-            if (!map.containsKey()) {
-                map.put(key, value);
+        DLinkedList node = cache.get(key);
+        if (node == null) {
+            DLinkedList newNode = new DLinkedList();
+            newNode.key = key;
+            newNode.value = value;
+            this.cache.put(key, newNode);
+            this.addNode(newNode);
+            count++;
+            if (count > capacity) {
+                DLinkedList tail = this.popTail();
+                cache.remove(tail.key);
+                count--;
             }
+        } else {
+            node.value = value;
+            this.moveToHead(node);
         }
     }
 }
