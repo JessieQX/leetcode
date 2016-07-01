49411cd157f7bd6436032eaa2816fd84de45674b
JessieQX <quxuan819@gmail.com>
2016-07-01 14:44:49 -0400

    [229. Majority Element II][Compile Error]committed by LeetCode Extension

---
+++ 229-Majority-Element-II/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        int i = 1;
+        int j = 2;
+        int major = nums[0];
+        int counter = 1;
+        while (i < n) {
+            if (major = nums[j++]) {
+                count++;
+            }
+            if (j >= n && count < n / 3) {
+                i = i + 1;
+                j = i + 1;
+                count = 1;
+            }
+            if (j < n && count >= n / 3) {
+                list.add(major);
+                i = i + 1;
+                j = i + 1;
+                count = 1;
+            }
+        }
+        return list;
+    }
+}
