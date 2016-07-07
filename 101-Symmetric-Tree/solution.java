e49b24fdeb5a1e7226f917532b4335dcfb25bb02
JessieQX <quxuan819@gmail.com>
2016-07-07 10:47:28 -0400

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -8,26 +8,18 @@
  * }
  */
 public class Solution {
-    // public boolean isSymmetric(TreeNode root) {
-    //     return root == null || isSymmetricHelper(root.left, root.right);
-    // }
-    // private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
-    //     if (left == null || right == null) {
-    //         return left == right;
-    //     }//if one node is null, the recursion should stop, so use || but not &&. left == right means the addresses are equal.
-    //      //if use true instead of left == right, the two nodes may have different values.
-    //     if (left.val != right.val) {
-    //         return false;
-    //     }
-    //     return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
-    // }
     public boolean isSymmetric(TreeNode root) {
-        if (root.left == null || root.right == null) {
-            return root.left == root.right;
-        }
-        if (root.left.val != root.right.val) {
+        return root == null || isSymmetricHelper(root.left, root.right);
+    }
+    private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }//if one node is null, the recursion should stop, so use || but not &&. left == right means the addresses are equal.
+         //if use true instead of left == right, the two nodes may have different values.
+        if (left.val != right.val) {
             return false;
         }
-        return isSymmetric(root.left) && isSymmetric(root.right);
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
     }
+    
 }
