4a253cad4639d04c77b87c2ec5c1faf54ea5df3f
JessieQX <quxuan819@gmail.com>
2016-07-08 17:49:12 -0400

    [347. Top K Frequent Elements][Compile Error]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -1,39 +1,42 @@
 public class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        // HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        // for(int num: nums){
-        //     map.put(num, map.containsKey(num)? map.get(num) + 1 : 1);
-        // }
-        // PriorityQueue<Map.Entry<Integer, Integer>> queue = 
-        // new PriorityQueue<Map.Entry<Integer, Integer>>(map.size(), (o1, o2) -> o2.getValue() - o1.getValue());
-        // queue.addAll(map.entrySet());
-        // List<Integer> res = new ArrayList<Integer>();
-        // for(int i = 0; i < k; i++){
-        //     res.add(queue.poll().getKey());
-        // }
-        // return res;
-        List<Integer>[] bucket = new List[nums.length + 1];
-	    Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int num: nums){
+            map.put(num, map.containsKey(num)? map.get(num) + 1 : 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = 
+        new PriorityQueue<Map.Entry<Integer, Integer>>(map.size(), (o1, o2) -> o2.getValue() - o1.getValue());
+        for (int i = 0; i < k; i++) {
+            queue.add(map.entrySet());
+        }
+        //queue.addAll(map.entrySet());
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < k; i++){
+            res.add(queue.poll().getKey());
+        }
+        return res;
+    //     List<Integer>[] bucket = new List[nums.length + 1];
+	   // Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
 
-	    for (int n : nums) {
-		    frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
-	    }
+	   // for (int n : nums) {
+		  //  frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+	   // }
 
-	    for (int key : frequencyMap.keySet()) {
-		    int frequency = frequencyMap.get(key);
-		    if (bucket[frequency] == null) {
-			    bucket[frequency] = new ArrayList<>();
-		    }
-		    bucket[frequency].add(key);
-	    }
+	   // for (int key : frequencyMap.keySet()) {
+		  //  int frequency = frequencyMap.get(key);
+		  //  if (bucket[frequency] == null) {
+			 //   bucket[frequency] = new ArrayList<>();
+		  //  }
+		  //  bucket[frequency].add(key);
+	   // }
 
-	    List<Integer> res = new ArrayList<>();
+	   // List<Integer> res = new ArrayList<>();
 
-	    for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
-		    if (bucket[pos] != null) {
-			    res.addAll(bucket[pos]);
-		    }
-	    }
-	    return res;
+	   // for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+		  //  if (bucket[pos] != null) {
+			 //   res.addAll(bucket[pos]);
+		  //  }
+	   // }
+	   // return res;
     }
 }
