2e8efbbdd36500c4c7bc6c7797c234c8ec1e5372
JessieQX <quxuan819@gmail.com>
2016-07-08 17:24:36 -0400

    [347. Top K Frequent Elements][Accepted]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -13,27 +13,27 @@ public class Solution {
         // }
         // return res;
         List<Integer>[] bucket = new List[nums.length + 1];
-	Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+	    Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
 
-	for (int n : nums) {
-		frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
-	}
+	    for (int n : nums) {
+		    frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+	    }
 
-	for (int key : frequencyMap.keySet()) {
-		int frequency = frequencyMap.get(key);
-		if (bucket[frequency] == null) {
-			bucket[frequency] = new ArrayList<>();
-		}
-		bucket[frequency].add(key);
-	}
+	    for (int key : frequencyMap.keySet()) {
+		    int frequency = frequencyMap.get(key);
+		    if (bucket[frequency] == null) {
+			    bucket[frequency] = new ArrayList<>();
+		    }
+		    bucket[frequency].add(key);
+	    }
 
-	List<Integer> res = new ArrayList<>();
+	    List<Integer> res = new ArrayList<>();
 
-	for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
-		if (bucket[pos] != null) {
-			res.addAll(bucket[pos]);
-		}
-	}
-	return res;
+	    for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+		    if (bucket[pos] != null) {
+			    res.addAll(bucket[pos]);
+		    }
+	    }
+	    return res;
     }
 }
