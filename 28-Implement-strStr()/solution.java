1f6fd68672931de106e8552e94c5ab36d31f3926
JessieQX <quxuan819@gmail.com>
2016-07-08 22:25:26 -0400

    [28. Implement strStr()][Runtime Error]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,6 +1,33 @@
 public class Solution {
     public int strStr(String haystack, String needle) {
-        int index = haystack.indexOf(needle);
-        return index;
+        // int index = haystack.indexOf(needle);
+        // return index;
+        if (haystack == null || needle == null || haystack.length() < needle.length()) {
+            return -1;
+        }
+        if (needle.length() == 0 || needle.equals(haystack)) {
+            return 0;
+        }
+        int len1 = needle.length();
+        int len2 = haystack.length();
+        int start = 0;
+        while (start < (len2 - len1)) {
+            int i = start;
+            for (int j = 0; j < len1; j++) {
+                Character c1 = needle.charAt(i);
+                Character c2 = haystack.charAt(i);
+                if (c1 != c2) {
+                    start++;
+                    break;
+                } else {
+                    if (j == len1 - 1) {
+                        return start; 
+                    } else {
+                        i++;
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
