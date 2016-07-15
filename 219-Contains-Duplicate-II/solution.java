937bf9b0954e7ba53aff843957e16202e8f5e7ed
JessieQX <quxuan819@gmail.com>
2016-07-14 20:50:28 -0400

    [219. Contains Duplicate II][Time Limit Exceeded]committed by LeetCode Extension

--- 219-Contains-Duplicate-II/solution.java
+++ 219-Contains-Duplicate-II/solution.java
@@ -7,7 +7,7 @@ public class Solution {
         }
         return false;
         
-//         Map<Integer, Integer> map = new HashMap<>();
+//      Map<Integer, Integer> map = new HashMap<>();
 // 		for (int i = 0; i < nums.length; i++) {
 // 			if (!map.containsKey(nums[i])) {
 // 				map.put(nums[i], i);
