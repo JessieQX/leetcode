86cc309f514c2a4aa62348c5b1c4ef63ae267c76
JessieQX <quxuan819@gmail.com>
2016-07-17 01:32:14 -0400

    [116. Populating Next Right Pointers in Each Node][Accepted]committed by LeetCode Extension

--- 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/solution.java
@@ -8,35 +8,44 @@
  */
 public class Solution {
     public void connect(TreeLinkNode root) {
-        if (root == null) return;
-        Queue<TreeLinkNode> queue = new LinkedList<>();
-        queue.offer(root);
+        // if (root == null) return;
+        // Queue<TreeLinkNode> queue = new LinkedList<>();
+        // queue.offer(root);
         
-        //root.next = null;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            Queue<TreeLinkNode> level = new LinkedList<>();
-            for (int i = 0; i < size; i++) {
-                TreeLinkNode head = queue.poll();
-                level.offer(head);
-                if (head.left != null) {
-                    queue.offer(head.left);
-                }
-                if (head.right != null) {
-                    queue.offer(head.right);
-                }
-            }
+        // //root.next = null;
+        // while (!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     Queue<TreeLinkNode> level = new LinkedList<>();
+        //     for (int i = 0; i < size; i++) {
+        //         TreeLinkNode head = queue.poll();
+        //         level.offer(head);
+        //         if (head.left != null) {
+        //             queue.offer(head.left);
+        //         }
+        //         if (head.right != null) {
+        //             queue.offer(head.right);
+        //         }
+        //     }
             
-            int size1 = level.size();
-            for (int i = 0; i < size1; i++) {
-                TreeLinkNode node = level.poll();
-                if (!level.isEmpty()) {
-                    node.next = level.peek();
-                } else {
-                    node.next = null;
-                }
+        //     int size1 = level.size();
+        //     for (int i = 0; i < size1; i++) {
+        //         TreeLinkNode node = level.poll();
+        //         if (!level.isEmpty()) {
+        //             node.next = level.peek();
+        //         } else {
+        //             node.next = null;
+        //         }
+        //     }
+        // }
+        // return;
+        while (root != null) {
+            TreeLinkNode p = root;
+            while (p != null && p.left != null) {
+                p.left.next = p.right;
+                p.right.next = p.next == null ? null : p.next.left;
+                p = p.next;
             }
+            root = root.left;
         }
-        return;
     }
 }
