d552d5eb0922be5d097ec0adf56fd674aea43cb7
JessieQX <quxuan819@gmail.com>
2016-07-16 21:23:56 -0400

    [110. Balanced Binary Tree][Accepted]committed by LeetCode Extension

---
+++ 110-Balanced-Binary-Tree/solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // public boolean isBalanced(TreeNode root) {
+    //     int result = maxDepth(root);
+    //     return result != -1;
+    // }
+    // private int maxDepth(TreeNode root) {
+    //     if (root == null) {
+    //         return 0;
+    //     }
+    //     int left = maxDepth(root.left);
+    //     int right = maxDepth(root.right);
+    //     if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+    //         return -1;
+    //     }
+    //     return Math.max(left, right) + 1;
+    // }
+    public boolean isBalanced(TreeNode root) {
+        int res = maxDepth(root);
+        if (res == -1) {
+            return false;
+        }
+        return true;
+    }
+    private int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
