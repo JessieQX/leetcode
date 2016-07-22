5f41abab759d79a030807c2f190ded0593d1d6d2
JessieQX <quxuan819@gmail.com>
2016-07-22 18:07:42 -0400

    [293. Flip Game][Compile Error]committed by LeetCode Extension

--- 293-Flip-Game/solution.java
+++ 293-Flip-Game/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         List<String> res = new ArrayList<>();
         for (int i = 1; i < s.length(); i++) {
             String ss = s;
-            if (s.charAt(i - 1) == s.charAt(i) == '+') {
+            if (s.charAt(i - 1) == s.charAt(i) && s.charAt(i) == '+') {
                 ss.charAt(i - 1) = '-';
                 ss.charAt(i) = '-';
                 res.add(ss);
