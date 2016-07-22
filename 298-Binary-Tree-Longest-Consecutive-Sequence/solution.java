ea387feaf25a2daf126216eb66e932a3033baf4b
JessieQX <quxuan819@gmail.com>
2016-07-22 00:37:34 -0400

    [298. Binary Tree Longest Consecutive Sequence][Runtime Error]committed by LeetCode Extension

---
+++ 298-Binary-Tree-Longest-Consecutive-Sequence/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private int max = 0;
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) return 0;
+        helper(root, 0, root.val);
+        return max;
+    }
+    private void helper(TreeNode root, int count, int target) {
+        if (root == null) return;
+        if (root.val == target) {
+            count++;
+        } else {
+            count = 1;
+        }
+        max = Math.max(count, max);
+        helper(root.left, count, root.left.val);
+        helper(root.right, count, root.right.val);
+    }
+}
