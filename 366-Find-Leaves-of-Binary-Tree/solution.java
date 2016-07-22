28e577b495efc807bc9e02355379b6e9562f209d
JessieQX <quxuan819@gmail.com>
2016-07-22 11:26:13 -0400

    [366. Find Leaves of Binary Tree][Runtime Error]committed by LeetCode Extension

---
+++ 366-Find-Leaves-of-Binary-Tree/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> findLeaves(TreeNode root) {
+        if (root == null) return res;
+        helper(root);
+        return res;
+    }
+    private int helper(TreeNode node) {
+        if (node == null) return -1;
+        int height = 1 + Math.max(helper(node.left), helper(node.right));
+        res.get(height).add(node.val);
+        return height;
+    }
+}
