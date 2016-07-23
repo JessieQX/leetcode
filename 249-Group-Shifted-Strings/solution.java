7200cdf7c5581813593062eab6749092a303706b
JessieQX <quxuan819@gmail.com>
2016-07-23 16:59:13 -0400

    [249. Group Shifted Strings][Compile Error]committed by LeetCode Extension

---
+++ 249-Group-Shifted-Strings/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strings) {
+            int shift = str.charAt(0) - 'a';
+            String key = "";
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i) - shift;
+                if (c < 'a') {
+                    c += 26;
+                }
+                key += c;
+            }
+            if (!map.containsKey(key)) {
+                List<String> list = new ArrayList<>();
+                map.put(key, list);
+            }
+            map.get(key).add(str);
+        }
+        for (String key : map.keySet()) {
+            List<String> list = map.get(key);
+            Collections.sort(list);
+            res.add(list);
+        }
+        return res;
+    }
+}
