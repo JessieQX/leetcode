f5e91b127d7477c2357850501e7d1e64fdb95976
JessieQX <quxuan819@gmail.com>
2016-08-02 13:36:13 -0400

    [167. Two Sum II - Input array is sorted][Runtime Error]committed by LeetCode Extension

---
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        if (numbers == null || numbers.length == 0) {
+            return res;
+        }
+        if (numbers[0] >= target) {
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (!map.containsKey(target - numbers[i])) {
+                map.put(numbers[i], i);
+            } else {
+                res[0] = map.get(target- numbers[i]);
+                res[1] = map.get(numbers[i]);
+            }
+        }
+        return res;
+    }
+}
