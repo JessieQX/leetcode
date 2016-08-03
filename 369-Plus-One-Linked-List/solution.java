3406b048edf46b3545c238eb3a7b8b0f5c2a42d9
JessieQX <quxuan819@gmail.com>
2016-08-03 12:39:38 -0400

    [369. Plus One Linked List][Compile Error]committed by LeetCode Extension

--- 369-Plus-One-Linked-List/solution.java
+++ 369-Plus-One-Linked-List/solution.java
@@ -7,35 +7,64 @@
  * }
  */
 public class Solution {
-    public ListNode plusOne(ListNode head) {
-        ListNode pointer = head;
-        ListNode dummy = new ListNode(0);
-        while (pointer.next != null) {
-            pointer = pointer.next;
-        }
-        helper(pointer, head, dummy);
-        if (dummy.val == 1) {
-            return dummy;
-        }
-        return head;
-    }
-    private void helper(ListNode pointer, ListNode head, ListNode dummy) {
-        if (pointer.val < 9) {
-            pointer.val = pointer.val + 1;
-            return; 
-        }
-        pointer.val = 0;
-        ListNode newPointer = null;
+    // public ListNode plusOne(ListNode head) {
+    //     ListNode pointer = head;
+    //     ListNode dummy = new ListNode(0);
+    //     while (pointer.next != null) {
+    //         pointer = pointer.next;
+    //     }
+    //     helper(pointer, head, dummy);
+    //     if (dummy.val == 1) {
+    //         return dummy;
+    //     }
+    //     return head;
+    // }
+    // private void helper(ListNode pointer, ListNode head, ListNode dummy) {
+    //     if (pointer.val < 9) {
+    //         pointer.val = pointer.val + 1;
+    //         return; 
+    //     }
+    //     pointer.val = 0;
+    //     ListNode newPointer = null;
         
-        if (pointer == head) {
-            dummy.next = head;
-            newPointer = dummy;
+    //     if (pointer == head) {
+    //         dummy.next = head;
+    //         newPointer = dummy;
+    //     } else {
+    //         newPointer = head;
+    //     }
+    //     while (newPointer.next != pointer) {
+    //         newPointer = newPointer.next;
+    //     }
+    //     helper(newPointer, head, dummy);
+    // }
+    ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        ListNode node = null;
+        while (cur.next != null) {
+            if (cur.val != 9) {
+                if (cur.next.val == 9) node = cur;
+                else node = null;
+            }
+            cur = cur.next;
+        }
+        if (node == null) {
+            cur.val ++;
+            return head;
+        } else if (node == dummy) {
+            dummy.val++;
+            while (head!= null) {
+                head.val = 0;
+                head = head.next;
+            }
+            return dummy;
         } else {
-            newPointer = head;
+            node.val++;
+            while (node.next!= null) {
+                node.next.val = 0;
+                node = node.next;
+            }
+            return head;
         }
-        while (newPointer.next != pointer) {
-            newPointer = newPointer.next;
-        }
-        helper(newPointer, head, dummy);
-    }
 }
