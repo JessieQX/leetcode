a3427ec261b0c1b5553d40e912a89b624db9fb0f
JessieQX <quxuan819@gmail.com>
2016-08-03 14:01:02 -0400

    [245. Shortest Word Distance III][Compile Error]committed by LeetCode Extension

---
+++ 245-Shortest-Word-Distance-III/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        List<Integer> index1 = new ArrayList<>();
+        List<Integer> index2 = new ArrayList<>();
+        int distance = words.length - 1;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                index1.add(i);
+            }
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word2)) {
+                index2.add(i);
+            }
+        }
+        for (int i = 0; i < index1.length(); i++) {
+            for (int j = 0; j < index2.length(); j++) {
+                if (i == j) {
+                    continue;
+                } else {
+                    distance = Math.min(distance, Math.abs(i - j));
+                }
+            }
+        }
+        return distance;
+    }
+}
