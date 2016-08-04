8edbc37aa30099e9c8672a87aa04350bbdfae5bf
JessieQX <quxuan819@gmail.com>
2016-08-04 17:13:59 -0400

    [311. Sparse Matrix Multiplication][Accepted]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -4,17 +4,28 @@ public class Solution {
         int colB = B[0].length;
         int M = A[0].length;
         int[][] AB = new int[rowA][colB];
-        for (int i = 0; i < rowA; i++) {
-            for (int j = 0; j < colB; j++) {
-                AB[i][j] = 0;
-            }
-        }
+        // for (int i = 0; i < rowA; i++) {
+        //     for (int j = 0; j < colB; j++) {
+        //         AB[i][j] = 0;
+        //     }
+        // }
 
+        // for (int i = 0; i < rowA; i++) {
+        //     for (int j = 0; j < colB; j++) {
+        //         for (int k = 0; k < M; k++) {
+        //             if (A[i][k] != 0 && B[k][j] != 0) {
+        //                 AB[i][j] += A[i][k] * B[k][j];
+        //             }
+        //         }
+        //     }
+        // }
         for (int i = 0; i < rowA; i++) {
-            for (int j = 0; j < colB; j++) {
-                for (int k = 0; k < M; k++) {
-                    if (A[i][k] != 0 && B[k][j] != 0) {
-                        AB[i][j] += A[i][k] * B[k][j];
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != 0) {
+                    for (int k = 0; k < colB; k++) {
+                        if (B[j][k] != 0) {
+                            AB[i][k] += A[i][j] * B[j][k];
+                        }
                     }
                 }
             }
