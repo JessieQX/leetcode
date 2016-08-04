bd3b4d776c4c9065e9e2db9a38605d17a7da278c
JessieQX <quxuan819@gmail.com>
2016-08-04 16:08:46 -0400

    [311. Sparse Matrix Multiplication][Runtime Error]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -4,6 +4,11 @@ public class Solution {
         int colB = B[0].length;
         int M = A[0].length;
         int[][] AB = new int[rowA - 1][colB - 1];
+        for (int i = 0; i < rowA; i++) {
+            for (int j = 0; i < colB; j++) {
+                AB[i][j] = 0;
+            }
+        }
         //int k = 0;
         for (int i = 0; i < rowA; i++) {
             for (int j = 0; j < colB; j++) {
