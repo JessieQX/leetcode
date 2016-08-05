bcc01af53a0c3c81e5230d5a2634ef506d731b60
JessieQX <quxuan819@gmail.com>
2016-08-05 00:17:46 -0400

    [294. Flip Game II][Accepted]committed by LeetCode Extension

--- 294-Flip-Game-II/solution.java
+++ 294-Flip-Game-II/solution.java
@@ -3,20 +3,14 @@ public class Solution {
         if (s == null || s.length() < 2) {
             return false;
         }
-        int count = helper(s, 1, 0);
-        if (count % 2 != 0) {
-            return true;
-        }
-        return false;
-    }
-    private int helper(String s, int start, int count) {
-        if (start == s.length() - 1) {
-            return count;
-        }
-        for (int i = start; i < s.length(); i++) {
+        for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i - 1) == '+' && s.charAt(i) == '+') {
-                helper(s.substring(0, i - 1) + "--" + s.substring(i + 1, s.length()), start + 1, count + 1);
+                String ss = s.substring(0, i - 1) + "--" + s.substring(i + 1, s.length());
+                if (!canWin(ss)) {
+                    return true;
+                }
             }
         }
+        return false;
     }
 }
