d973eda1f730d89178ac0f57822571ee6571514a
JessieQX <quxuan819@gmail.com>
2016-08-12 19:16:40 -0400

    [47. Permutations II][Compile Error]committed by LeetCode Extension

--- 47-Permutations-II/solution.java
+++ 47-Permutations-II/solution.java
@@ -1,30 +1,28 @@
 public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return res;
-        }
+           public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
         List<Integer> list = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
         Arrays.sort(nums);
-        permuteHelper(res, list, nums, visited);
+        permuteHelper(nums, res, list, visited);
         return res;
     }
-    private void permuteHelper(List<List<Integer>> res, List<Integer> list, int[] nums, boolean[] visited) {
-        if (nums.length == list.size()) {
+    private void permuteHelper(int[] nums, List<List<Integer>> res, List<Integer> list, boolean[] visited) {
+        if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
             if (visited[i]) continue;
-            visited[i] = true;
+            if (i > 0 && nums[i - 1] == nums[i] && !visited[i - 1]) continue;
             list.add(nums[i]);
-            permuteHelper(res, list, nums, visited);
+            visited[i] = true;
+            permuteHelper(nums, res, list, visited);
             visited[i] = false;
             list.remove(list.size() - 1);
-            while (i < nums.length - 1 && nums[i] == nums[i + 1]) {
-                i++;
-            }
         }
     }
 }
