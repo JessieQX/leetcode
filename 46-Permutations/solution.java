12266e3ebb54509b34f163ad8ac7d98b328535bc
JessieQX <quxuan819@gmail.com>
2016-08-12 19:18:45 -0400

    [46. Permutations][Compile Error]committed by LeetCode Extension

---
+++ 46-Permutations/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        List<Integer> list = new ArrayList<>();
+        //boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        permuteHelper(nums, res, list);
+        return res;
+    }
+    private void permuteHelper(int[] nums, List<List<Integer>> res, List<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            //if (visited[i]) continue;
+            //if (i > 0 && nums[i - 1] == nums[i] && !visited[i - 1]) continue;
+            list.add(nums[i]);
+            //visited[i] = true;
+            permuteHelper(nums, res, list);
+            //visited[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+}
