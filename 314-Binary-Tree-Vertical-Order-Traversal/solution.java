986a5b902318fb5b056e00173ba3f57e8f4ac297
JessieQX <quxuan819@gmail.com>
2016-08-12 09:08:12 -0400

    [314. Binary Tree Vertical Order Traversal][Accepted]committed by LeetCode Extension

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -9,69 +9,35 @@
  */
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        // List<List<Integer>> res = new ArrayList<>();
-        // Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-        // Queue<TreeNode> queue = new LinkedList<>();
-        // Queue<Integer> cols = new LinkedList<>();
-        // queue.offer(root);
-        // cols.offer(0);
-        // int min = 0, max = 0;
-        // while (!queue.isEmpty()) {
-        //     TreeNode node = queue.poll();
-        //     int col = cols.poll();
-        //     if (!map.containsKey(col)) {
-        //         map.put(col, new ArrayList<Integer>());
-        //     }
-        //     map.get(col).add(node.val);
-        //     if (node.left != null) {
-        //         queue.offer(node.left);
-        //         cols.offer(col - 1);
-        //         if (col <= min) min = col - 1;
-        //     }
-        //     if (node.right != null) {
-        //         queue.offer(node.right);
-        //         cols.offer(col + 1);
-        //         if (col >= max) max = col + 1;
-        //     }
-        // }
-        // for (int i = min; i <= max; i++) {
-        //     res.add(map.get(i));
-        // }
-        // return res;
-        
         List<List<Integer>> res = new ArrayList<>();
-	if(root == null) return res;
-	
-	Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-	Queue<TreeNode> q = new LinkedList<>();
-	Queue<Integer> cols = new LinkedList<>();
-
-	q.add(root); 
-	cols.add(0);
-
-	int min = 0, max = 0;
-	while(!q.isEmpty()) {
-		TreeNode node = q.poll();
-		int col = cols.poll();
-		if(!map.containsKey(col)) map.put(col, new ArrayList<Integer>());
-		map.get(col).add(node.val);
-
-		if(node.left != null) {
-			q.add(node.left); 
-			cols.add(col - 1);
-			if(col <= min) min = col - 1;
-		}
-		if(node.right != null) {
-			q.add(node.right);
-			cols.add(col + 1);
-			if(col >= max) max = col + 1;
-		}
-	}
-
-	for(int i = min; i <= max; i++) {
-		res.add(map.get(i));
-	}
-
-	return res;
+        if (root == null) return res;
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> cols = new LinkedList<>();
+        queue.offer(root);
+        cols.offer(0);
+        int min = 0, max = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            int col = cols.poll();
+            if (!map.containsKey(col)) {
+                map.put(col, new ArrayList<Integer>());
+            }
+            map.get(col).add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+                cols.offer(col - 1);
+                if (col <= min) min = col - 1;
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+                cols.offer(col + 1);
+                if (col >= max) max = col + 1;
+            }
+        }
+        for (int i = min; i <= max; i++) {
+            res.add(map.get(i));
+        }
+        return res;
     }
 }
