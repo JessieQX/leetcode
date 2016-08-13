2331f91797980ef48d5da6cc3199192854f9f6f5
JessieQX <quxuan819@gmail.com>
2016-08-13 11:09:03 -0400

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,30 +1,29 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-    // Arrays.sort(nums);
-    // int totalNumber = 1 << nums.length;
-    // List<List<Integer>> collection = new ArrayList<List<Integer>>(totalNumber);
-    // for (int i=0; i<totalNumber; i++) {
-    //     List<Integer> set = new LinkedList<Integer>();
-    //     for (int j=0; j<nums.length; j++) {
-    //         if ((i & (1<<j)) != 0) {
-    //             set.add(nums[j]);
-    //         }
-    //     }
-    //     collection.add(set);
-    // }
-    // return collection;
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
+        // List<List<Integer>> res = new ArrayList<>();
+        // res.add(new ArrayList<>());
+        // Arrays.sort(nums);
+        // for (int i : nums) {
+        //     List<List<Integer>> tmp = new ArrayList<>();
+        //     for (List<Integer> sub : res) {
+        //         List<Integer> a = new ArrayList<>(sub);
+        //         a.add(i);
+        //         tmp.add(a);
+        //     }
+        //     res.addAll(tmp);
+        // }
+        // return res;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i : nums) {
-            List<List<Integer>> tmp = new ArrayList<>();
-            for (List<Integer> sub : res) {
-                List<Integer> a = new ArrayList<>(sub);
-                a.add(i);
-                tmp.add(a);
-            }
-            res.addAll(tmp);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
         }
-        return res;
     }
 }
