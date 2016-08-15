f8d62c8da083ba129b397097f7c04285bb3ed38a
JessieQX <quxuan819@gmail.com>
2016-08-14 21:34:02 -0400

    [114. Flatten Binary Tree to Linked List][Accepted]committed by LeetCode Extension

--- 114-Flatten-Binary-Tree-to-Linked-List/solution.java
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution.java
@@ -8,37 +8,37 @@
  * }
  */
 public class Solution {
-    // private TreeNode prev = null;
-    // public void flatten(TreeNode root) {
-    //     if (root == null) {
-    //         return;
-    //     }
-    //     //reverse pre-order recursion. find the right-est node and make it point to null. 
-    //     //Then make the right-est node as prev, make the last left node as root pointing to prev.
-    //     //Then make the root as prev, and find the last root as new root.
-    //     flatten(root.right);
-    //     flatten(root.left);
-    //     root.right = prev;
-    //     root.left = null;
-    //     prev = root;
-    // }
+    private TreeNode prev = null;
     public void flatten(TreeNode root) {
-		if (root == null) {
+        if (root == null) {
             return;
         }
-        
-        TreeNode left = root.left;
-		TreeNode right = root.right;
-
-		flatten(left);
-		flatten(right);
-
+        //reverse pre-order recursion. find the right-est node and make it point to null. 
+        //Then make the right-est node as prev, make the last left node as root pointing to prev.
+        //Then make the root as prev, and find the last root as new root.
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
         root.left = null;
-		root.right = left;
-		TreeNode node = root;
-		while (node.right != null) {
-			node = node.right;
-		}
-		node.right = right;
-	}
+        prev = root;
+    }
+//     public void flatten(TreeNode root) {
+// 		if (root == null) {
+//             return;
+//         }
+        
+//         TreeNode left = root.left;
+// 		TreeNode right = root.right;
+
+// 		flatten(left);
+// 		flatten(right);
+
+//         root.left = null;
+// 		root.right = left;
+// 		TreeNode node = root;
+// 		while (node.right != null) {
+// 			node = node.right;
+// 		}
+// 		node.right = right;
+// 	}
 }
