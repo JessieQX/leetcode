f7ccf8e750cec4119e01d99150282c36d6a7363e
JessieQX <quxuan819@gmail.com>
2016-08-16 11:23:12 -0400

    [131. Palindrome Partitioning][Compile Error]committed by LeetCode Extension

--- 131-Palindrome-Partitioning/solution.java
+++ 131-Palindrome-Partitioning/solution.java
@@ -1,9 +1,6 @@
 public class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
-        List<String> list = new ArrayList<>();
-        helper(s, res, list, 0);
-        return res;
     }
     private void helper(String s, List<List<String>> res, List<String> list, int start) {
         if (start == s.length()) {
@@ -11,14 +8,14 @@ public class Solution {
             return;
         }
         for (int i = start; i < s.length(); i++) {
-            if (isPalindrome(s, start, i)) {
-                list.add(s.substring(start, i + 1));
-                helper(s, res, list, i + 1);
-                list.remove(list.size() - 1);
-            }
+            if (!isPalindrome(s.substring(start, i + 1))) continue;
+            list.add(s.substring(start, i + 1));
+            helper(s, res, list, i + 1);
+            list.remove(list.size() - 1);
         }
     }
-    private boolean isPalindrome (String s, int start, int end) {
+    private boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
         while (start < end) {
             if (s.charAt(start) != s.charAt(end)) {
                 return false;
