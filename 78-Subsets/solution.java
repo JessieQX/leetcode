97eeff3db9631c7787b7226b4fc4826302daa81b
JessieQX <quxuan819@gmail.com>
2016-08-16 00:12:44 -0400

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,30 +1,15 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        // List<List<Integer>> res = new ArrayList<>();
-        // res.add(new ArrayList<>());
-        // Arrays.sort(nums);
-        // for (int i : nums) {
-        //     List<List<Integer>> tmp = new ArrayList<>();
-        //     for (List<Integer> sub : res) {
-        //         List<Integer> a = new ArrayList<>(sub);
-        //         a.add(i);
-        //         tmp.add(a);
-        //     }
-        //     res.addAll(tmp);
-        // }
-        // return res;
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> tempList = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(list, tempList, nums, 0);
-        return list;
+        List<List<Integer>> res = new ArrayList<>();
+        helper(nums, res, new ArrayList<>(), 0);
+        return res;
     }
-    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-        list.add(new ArrayList<>(tempList));
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, i + 1);
-            tempList.remove(tempList.size() - 1);
+    private void helper(int[] nums, List<List<Integer>> res, List<Integer> list, int start) {
+        res.add(new ArrayList<>(list));
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            helper(nums, res, list, i + 1);
+            list.remove(list.size() - 1);
         }
     }
 }
