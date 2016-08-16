ceb9a0dd33768be079a4597991fcbbc516a988ea
JessieQX <quxuan819@gmail.com>
2016-08-16 09:46:48 -0400

    [39. Combination Sum][Compile Error]committed by LeetCode Extension

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        helper(result, curr, candidates, target, 0);
-        return result;
+        helper(candidates, res, new ArrayList<>(), target, 0);
     }
-    private void helper(List<List<Integer>> result, List<Integer> curr, int[] candidates, int target, int start) {
-        if (target > 0) {
-            for (int i = start; i < candidates.length && target >= candidates[i]; i++) {
-                curr.add(candidates[i]);
-                helper(result, curr, candidates, target - candidates[i], i);
-                curr.remove(curr.size() - 1);
+    private void helper(int[] candidates, List<List<Integer>> res, List<Integer> list, int target, int start) {
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        } else if (target > 0) {
+            for (int i = start; i < candidates.length && nums[i] <= target; i++) {
+                if (i > start && candidates[i] == candidates[i - 1]) continue;
+                list.add(candidates[i]);
+                helper(candidates, res, list, target - nums[i], i);
+                list.remove(list.size() - 1);
             }
-        } else if (target == 0) {
-            result.add(new ArrayList<Integer>(curr));
         }
     }
 }
