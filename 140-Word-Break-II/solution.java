8e9557af4ca3a530c831717c162e2dd8a0d622d0
JessieQX <quxuan819@gmail.com>
2016-08-17 20:56:05 -0400

    [140. Word Break II][Accepted]committed by LeetCode Extension

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,21 +1,26 @@
 public class Solution {
     public List<String> wordBreak(String s, Set<String> wordDict) {
-        List<String> res = new ArrayList<>();
-        helper(s, wordDict, res, "", 0);
-        return res;
-    }
-    private void helper(String s, Set<String> wordDict, List<String> res, String str, int start) {
-        if (start == s.length()) {
-            res.add(new String(str.substring(0, str.length() - 1)));
-            return;
-        }
-        for (int i = start; i < s.length(); i++) {
-            if (wordDict.contains(s.substring(start, i + 1))) {
-                str += s.substring(start, i + 1) + " ";
-                helper(s, wordDict, res, str, i + 1);
-                str -= (s.substring(start, i + 1) + " "); 
+        return DFS(s, wordDict, new HashMap<String, LinkedList<String>>());
+    }       
+
+    // DFS function returns an array including all substrings derived from s.
+    List<String> DFS(String s, Set<String> wordDict, HashMap<String, LinkedList<String>>map) {
+        if (map.containsKey(s)) 
+            return map.get(s);
+        
+        LinkedList<String>res = new LinkedList<String>();     
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }               
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String>sublist = DFS(s.substring(word.length()), wordDict, map);
+                for (String sub : sublist) 
+                    res.add(word + (sub.isEmpty() ? "" : " ") + sub);               
             }
-            
-        }
+        }       
+        map.put(s, res);
+        return res;
     }
 }
