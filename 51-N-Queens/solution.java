847f4a22259ea228fcdf3b59fb2fa9c623521a05
JessieQX <quxuan819@gmail.com>
2016-08-23 00:07:10 -0400

    [51. N-Queens][Accepted]committed by LeetCode Extension

--- 51-N-Queens/solution.java
+++ 51-N-Queens/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     
-    public List<String[]> solveNQueens(int n) {
+    public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
         for(int i = 0; i < n; i++)
             for(int j = 0; j < n; j++)
