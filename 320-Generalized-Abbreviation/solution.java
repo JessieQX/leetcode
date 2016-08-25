9f2c6f713170c53e8ad0f751b5e64ee9004b85ed
JessieQX <quxuan819@gmail.com>
2016-08-24 21:49:31 -0400

    [320. Generalized Abbreviation][Accepted]committed by LeetCode Extension

--- 320-Generalized-Abbreviation/solution.java
+++ 320-Generalized-Abbreviation/solution.java
@@ -1,15 +1,18 @@
 public class Solution {
     public List<String> generateAbbreviations(String word) {
         List<String> res = new ArrayList<>();
-        return helper(res, word, "", 0, 0);
+        helper(res, word, "", 0, 0);
+        return res;
     }
     private void helper(List<String> res, String word, String curr, int pos, int count) {
         if (pos == word.length()) {
-            curr = curr + count;
+            if (count > 0) {
+                curr = curr + count;
+            }
             res.add(curr);
         } else {
             helper(res, word, curr, pos + 1, count + 1);
-            helper(res, word, curr + (count > 0 ? count : ""), pos + 1, 0);
+            helper(res, word, curr + (count > 0 ? count : "") + word.charAt(pos), pos + 1, 0);
         }
     }
 }
