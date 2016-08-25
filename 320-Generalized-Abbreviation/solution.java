71e0631b1b1a4b9eb01255d315255247e0e372bb
JessieQX <quxuan819@gmail.com>
2016-08-24 21:43:48 -0400

    [320. Generalized Abbreviation][Compile Error]committed by LeetCode Extension

---
+++ 320-Generalized-Abbreviation/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<String> generateAbbreviations(String word) {
+        List<String> res = new ArrayList<>();
+        return helper(res, word, "", 0, 0);
+    }
+    private List<String> helper(List<String> res, String word, String curr, int pos, int count) {
+        if (pos == word.length()) {
+            curr = curr + count;
+            res.add(curr);
+        } else {
+            helper(res, word, curr, pos + 1, count + 1);
+            helper(res, word, curr + count > 0 ? count : "", pos + 1, 0);
+        }
+    }
+}
