ff7f16da50980ad5989dbe938ef1d360c3cd3e47
JessieQX <quxuan819@gmail.com>
2016-08-25 16:16:49 -0400

    [163. Missing Ranges][Compile Error]committed by LeetCode Extension

--- 163-Missing-Ranges/solution.java
+++ 163-Missing-Ranges/solution.java
@@ -1,32 +1,35 @@
 public class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> res = new ArrayList<>();
-        if (lower > nums[0] || upper < nums[nums.length - 1]) {
+        if (nums.isEmpty() && lower == upper) {
+            res.add(lower + "");
+            return res;
+        }
+        if (nums == null || nums.length == 0 || lower > nums[0] || upper < nums[nums.length - 1]) {
             return res;
         }
         if (lower + 1 < nums[0]) {
             if ((lower + 1) == (nums[0] - 1)) {
-                res.add(lower + 1 + "");
+                res.add((lower + 1) + "");
             } else {
                 res.add((lower + 1) + "->" + (nums[0] - 1));
             }
-        } else if ((upper - 1) > nums[nums.length - 1]) {
-            if ((upper - 1) == (nums[nums.length - 1] + 1)) {
-                res.add((upper - 1) + "");
+        } 
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] == 1) {
+                continue;
+            } else if (nums[i] - nums[i - 1] == 2) {
+                res.add((nums[i] - 1) + "");
             } else {
-                res.add((upper - 1) + "->" + (nums[nums.length - 1] + 1));
-            }
-        } else {
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] - nums[i - 1] == 1) {
-                    continue;
-                } else if (nums[i] - nums[i - 1] == 2) {
-                    res.add((nums[i] - 1) + "");
-                } else {
-                    res.add((nums[i - 1] + 1) + "->" + (nums[i] - 1));
-                }
+                res.add((nums[i - 1] + 1) + "->" + (nums[i] - 1));
             }
         }
+        if ((upper - 1) > nums[nums.length - 1]) {
+            
+            res.add((nums[nums.length - 1] + 1) + "->" + upper);
+            
+        } 
         return res;
     }
 }
