237c5b1c12876c00d4e5db40545ac5e6aa0fdc4c
JessieQX <quxuan819@gmail.com>
2016-08-29 10:00:55 -0400

    [259. 3Sum Smaller][Accepted]committed by LeetCode Extension

--- 259-3Sum-Smaller/solution.java
+++ 259-3Sum-Smaller/solution.java
@@ -1,21 +1,40 @@
 public class Solution {
+    // public int threeSumSmaller(int[] nums, int target) {
+    //     if (nums == null || nums.length == 0) return 0;
+    //     Arrays.sort(nums);
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     List<Integer> list = new ArrayList<>();
+    //     helper(nums, target, 0, list, res);
+    //     return res.size();
+    // }
+    // private void helper(int[] nums, int target, int start, List<Integer> list, List<List<Integer>> res) {
+    //     if (list.size() == 3 && target > 0) {
+    //         res.add(new ArrayList<>(list));
+    //         return;
+    //     }
+    //     for (int i = start; i < nums.length; i++) {
+    //         list.add(nums[i]);
+    //         helper(nums, target - nums[i], i + 1, list, res);
+    //         list.remove(list.size() - 1);
+    //     }
+    // }
     public int threeSumSmaller(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        helper(nums, target, 0, list, res);
-        return res.size();
-    }
-    private void helper(int[] nums, int target, int start, List<Integer> list, List<List<Integer>> res) {
-        if (list.size() == 3 && target > 0) {
-            res.add(new ArrayList<>(list));
-            return;
+        int count = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] < target) {
+                    count += right - left;
+                    left++;
+                } else {
+                    right--;
+                }
+            }
         }
-        for (int i = start; i < nums.length; i++) {
-            list.add(nums[i]);
-            helper(nums, target - nums[i], i + 1, list, res);
-            list.remove(list.size() - 1);
-        }
+        return count;
     }
 }
