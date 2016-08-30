a8bcd0eb1ccf8ff8faa1ff93e37fb3efda77bea4
JessieQX <quxuan819@gmail.com>
2016-08-30 13:37:16 -0400

    [261. Graph Valid Tree][Compile Error]committed by LeetCode Extension

---
+++ 261-Graph-Valid-Tree/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        int[] nums = new nums[n];
+        Arrays.fill(nums, -1);
+        for (int[] edge : edges) {
+            int x = findUnion(nums, edge[0]);
+            int y = findUnion(nums, edge[1]);
+            if (x == y) return false;
+            nums[x] = y;
+        }
+        return edge.length == n - 1;
+    }
+    private int findUnion(int[] nums, int i) {
+        if (nums[i] == -1) {
+            return i;
+        }
+        return findUnion(nums, nums[i]);
+    }
+}
