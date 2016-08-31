9802734d1f803b5dffc44d4901c83d7f4998a7a5
JessieQX <quxuan819@gmail.com>
2016-08-30 23:11:29 -0400

    [389. Find the Difference][Compile Error]committed by LeetCode Extension

---
+++ 389-Find-the-Difference/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+        for (int j = 0; j < t.length(); j++) {
+            char c = t.charAt(j);
+            if (!map.containsKey(c)) {
+                return c;
+            } else {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0) {
+                    map.remove(c);
+                }
+            }
+        }
+        return null;
+    }
+}
