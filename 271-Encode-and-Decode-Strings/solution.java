5430e5392f88cd39b44177e1d90b854c82ba2237
JessieQX <quxuan819@gmail.com>
2016-08-31 17:51:29 -0400

    [271. Encode and Decode Strings][Accepted]committed by LeetCode Extension

---
+++ 271-Encode-and-Decode-Strings/solution.java
@@ -0,0 +1,28 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s.length()).append('/').append(s);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < s.length()) {
+            int index = s.indexOf('/', i);
+            int size = Integer.parseInt(s.substring(i, index));
+            res.add(s.substring(index + 1, index + size + 1));
+            i = index + size + 1;
+        }
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
