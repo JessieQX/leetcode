63d85bd66027848d336e949dca60ab607cf60a5b
JessieQX <quxuan819@gmail.com>
2016-08-30 23:20:30 -0400

    [389. Find the Difference][Accepted]committed by LeetCode Extension

--- 389-Find-the-Difference/solution.java
+++ 389-Find-the-Difference/solution.java
@@ -1,27 +1,39 @@
 public class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
-        char res = ' ';
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!map.containsKey(c)) {
-                map.put(c, 1);
-            } else {
-                map.put(c, map.get(c) + 1);
+        // Map<Character, Integer> map = new HashMap<>();
+        // char res = ' ';
+        // for (int i = 0; i < s.length(); i++) {
+        //     char c = s.charAt(i);
+        //     if (!map.containsKey(c)) {
+        //         map.put(c, 1);
+        //     } else {
+        //         map.put(c, map.get(c) + 1);
+        //     }
+        // }
+        // for (int j = 0; j < t.length(); j++) {
+        //     char c = t.charAt(j);
+        //     if (!map.containsKey(c)) {
+        //         res = c;
+        //         break;
+        //     } else {
+        //         map.put(c, map.get(c) - 1);
+        //         if (map.get(c) == 0) {
+        //             map.remove(c);
+        //         }
+        //     }
+        // }
+        // return res;
+        int[] res = new int[26];
+        for (int i = 0; i < 26; i++) res[i] = 0;
+        for (char c : s.toCharArray()) {
+            res[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            res[c - 'a']--;
+            if (res[c - 'a'] < 0) {
+                return c;
             }
         }
-        for (int j = 0; j < t.length(); j++) {
-            char c = t.charAt(j);
-            if (!map.containsKey(c)) {
-                res = c;
-                break;
-            } else {
-                map.put(c, map.get(c) - 1);
-                if (map.get(c) == 0) {
-                    map.remove(c);
-                }
-            }
-        }
-        return res;
+        return 0;
     }
 }
