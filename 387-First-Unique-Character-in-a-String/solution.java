b59e2a7b8fb8dbb1c740807914f1059c7261886b
JessieQX <quxuan819@gmail.com>
2016-08-31 00:27:13 -0400

    [387. First Unique Character in a String][Runtime Error]committed by LeetCode Extension

--- 387-First-Unique-Character-in-a-String/solution.java
+++ 387-First-Unique-Character-in-a-String/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int firstUniqChar(String s) {
         if (s == null || s.length() == 0) return -1;
         int[] res = new int[26];
-        for (int i = 0; i < res; i++) {
+        for (int i = 0; i < 26; i++) {
             res[i] = 0;
         }
         
