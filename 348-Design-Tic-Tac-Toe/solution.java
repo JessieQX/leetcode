0d44f5a6302e5c72e69647e112d5dae78a3781d6
JessieQX <quxuan819@gmail.com>
2016-08-31 20:25:18 -0400

    [348. Design Tic-Tac-Toe][Compile Error]committed by LeetCode Extension

--- 348-Design-Tic-Tac-Toe/solution.java
+++ 348-Design-Tic-Tac-Toe/solution.java
@@ -24,7 +24,7 @@ public class TicTacToe {
         if (row == col) {
             diagonal += toAdd;
         }
-        if (col == (col.length - row - 1)) {
+        if (col == (n - row - 1)) {
             anti_diagonal += toAdd;
         }
         int size = row.length;
