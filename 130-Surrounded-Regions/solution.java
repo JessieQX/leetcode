429364849d0750a7d8514ca211864b9aa2021e97
JessieQX <quxuan819@gmail.com>
2016-09-01 19:50:48 -0400

    [130. Surrounded Regions][Runtime Error]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -24,25 +24,25 @@ public class Solution {
     private void helper(char[][] board, int i, int j, int m, int n) {
         if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') return;
         board[i][j] = '1';
-        // if (i - 1 >= 0 && board[i - 1][j] == 'O') {
-        //     //board[i - 1][j] = '1';
-        //     helper(board, i - 1, j, m, n);
-        // }
-        // if (i + 1 < m && board[i + 1][j] == 'O') {
-        //     //board[i + 1][j] = '1';
-        //     helper(board, i + 1, j, m, n);
-        // }
-        // if (j - 1 >= 0 && board[i][j - 1] == 'O') {
-        //     //board[i][j - 1] = '1';
-        //     helper(board, i, j - 1, m, n);
-        // }
-        // if (j + 1 < n && board[i][j + 1] == 'O') {
-        //     //board[i][j + 1] = '1';
-        //     helper(board, i, j + 1, m, n);
-        // }
-        helper(board, i - 1, j, m, n);
-        helper(board, i + 1, j, m, n);
-        helper(board, i, j - 1, m, n);
-        helper(board, i, j + 1, m, n);
+        if (i - 1 >= 0 && board[i - 1][j] == 'O') {
+            //board[i - 1][j] = '1';
+            helper(board, i - 1, j, m, n);
+        }
+        if (i + 1 < m && board[i + 1][j] == 'O') {
+            //board[i + 1][j] = '1';
+            helper(board, i + 1, j, m, n);
+        }
+        if (j - 1 >= 0 && board[i][j - 1] == 'O') {
+            //board[i][j - 1] = '1';
+            helper(board, i, j - 1, m, n);
+        }
+        if (j + 1 < n && board[i][j + 1] == 'O') {
+            //board[i][j + 1] = '1';
+            helper(board, i, j + 1, m, n);
+        }
+        // helper(board, i - 1, j, m, n);
+        // helper(board, i + 1, j, m, n);
+        // helper(board, i, j - 1, m, n);
+        // helper(board, i, j + 1, m, n);
     }
 }
