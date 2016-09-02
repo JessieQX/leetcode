9b409f59890fccf99a410bf871ded92132813a6a
JessieQX <quxuan819@gmail.com>
2016-09-01 20:24:01 -0400

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -16,21 +16,21 @@ public class Solution {
         return count;
     }
     private void helper(char[][] grid, int i, int j, int m, int n) {
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != '1') {
-            return;
-        }
-        grid[i][j] = '0';
-        helper(grid, i + 1, j, m, n);
-        helper(grid, i - 1, j, m, n);
-        helper(grid, i, j + 1, m, n);
-        helper(grid, i, j - 1, m, n);
-        // if (grid[i][j] == '1') {
-        //     grid[i][j] = '0';
-        //     if (i > 1) helper(grid, i - 1, j, m, n);
-        //     if (i + 1 < m) helper(grid, i + 1, j, m, n);
-        //     if (j > 1) helper(grid, i, j - 1, m, n);
-        //     if (j + 1 < n) helper(grid, i, j + 1, m, n);
+        // if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != '1') {
+        //     return;
         // }
-        // return;
+        // grid[i][j] = '0';
+        // helper(grid, i + 1, j, m, n);
+        // helper(grid, i - 1, j, m, n);
+        // helper(grid, i, j + 1, m, n);
+        // helper(grid, i, j - 1, m, n);
+        if (grid[i][j] == '1') {
+            grid[i][j] = '0';
+            if (i >= 1) helper(grid, i - 1, j, m, n);
+            if (i + 1 < m) helper(grid, i + 1, j, m, n);
+            if (j >= 1) helper(grid, i, j - 1, m, n);
+            if (j + 1 < n) helper(grid, i, j + 1, m, n);
+        }
+        return;
     }
 }
