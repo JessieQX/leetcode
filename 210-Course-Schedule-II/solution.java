5393e373f85271268c8b90f3e0182f8a57ed9cec
JessieQX <quxuan819@gmail.com>
2016-09-02 13:58:22 -0400

    [210. Course Schedule II][Compile Error]committed by LeetCode Extension

---
+++ 210-Course-Schedule-II/solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adjs = new ArrayList<>(numCourses);
+        int[] indegree = new int[numCourses];
+        initializeGraph(adjs, indegree, prerequisites);
+        return solveByBFS(indegree, adjs);
+    }
+    private void initializeGraph(List<List<Integer>> adjs, int[] indegree, int[][] prerequisites) {
+        int n = indegree.length;
+        while (n-- > 0) {
+            adjs.add(new ArrayList<>());
+        }
+        for (int[] edge : prerequisites) {
+            indegree[edge[0]]++;
+            adjs.get(edge[1]).add(edge[0]);
+        }
+    }
+    private int solveByBFS(int[] indegree, List<List<Integer>> adjs) {
+        int[] order = new int[indegree.length];
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            order[index++] = pre;
+            for (int post : adjs.get(pre)) {
+                if (--indegree[post] == 0) {
+                    queue.offer(post);
+                }
+            }
+        }
+        return indegree.length == index ? order : new int[0];
+    }
+}
