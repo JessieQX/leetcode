b48a2cb0d41306bd8f17461fe385688dd55136df
JessieQX <quxuan819@gmail.com>
2016-09-02 13:12:34 -0400

    [207. Course Schedule][Compile Error]committed by LeetCode Extension

---
+++ 207-Course-Schedule/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[][] matrix = new int[numCourses][numCourses];
+        int[] indegree = new int[numCourses];
+        for (int[] i : prerequisites) {
+            int pre = i[1];
+            int post = i[0];
+            if (matrix[pre][post] == 0) {
+                indegree[post]++;
+            }
+            matrix[pre][post] = 1;
+        }
+        
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) queue.offer(i);
+        }
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            for (int i = 0; i < numCourses; i++) {
+                if (matrix[course][i] != 0) {
+                    if (--indegree[i] == 0) {
+                        queue.offer(i);
+                    }
+                }
+            }
+        }
+        return count == numCourses
+    }
+}
