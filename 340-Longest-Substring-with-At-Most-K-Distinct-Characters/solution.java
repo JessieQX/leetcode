aa99eac1c8bc104a843969389b71f0ac0e7f7a44
JessieQX <quxuan819@gmail.com>
2016-09-03 13:36:32 -0400

    [340. Longest Substring with At Most K Distinct Characters][Accepted]committed by LeetCode Extension

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -1,34 +1,25 @@
 public class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if (s == null || s.length() == 0 || k == 0) return 0;
-        if (s.length() == 1) return 1;
-        int sum = 0;
-        for (int i = 0; i < s.length() - 1; i++) {
-            String res = getSubstring(s, k, i);
-            sum = Math.max(sum, res.length());
-        }
-        return sum;
-    }
-    private String getSubstring(String s, int k, int start) {
-        String res = "";
-        int count = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (int i = start; i < s.length(); i++) {
+        int left = 0;
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (count > k) break;
             if (!map.containsKey(c)) {
                 map.put(c, 1);
-                count++;
-                if (count <= k) {
-                    res = s.substring(start, i + 1);
-                }
             } else {
                 map.put(c, map.get(c) + 1);
-                if (count <= k) {
-                    res = s.substring(start, i + 1);
-                }
             }
+            while (map.size() > k) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+                left++;
+            }
+            sum = Math.max(sum, i - left + 1);
         }
-        return res;
+        return sum;
     }
 }
