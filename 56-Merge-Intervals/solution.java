80219d5db323dfb4963cd765b447629cb1c6b751
JessieQX <quxuan819@gmail.com>
2016-09-04 09:58:24 -0400

    [56. Merge Intervals][Accepted]committed by LeetCode Extension

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -11,27 +11,9 @@ public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> res = new ArrayList<>();
         if (intervals == null || intervals.size() == 0) return res;
-        //List<List<Integer>> occupation = new ArrayList<>();
-        // for (Interval interval : intervals) {
-        //     occupation.add(new ArrayList<>(Arrays.asList(interval.start, interval.end)));
-        // }
         Collections.sort(intervals, (a, b) -> a.start - b.start);
-        //occupation.sort((a, b) -> a.get(0) - b.get(0));
-        //List<Integer> pre = occupation.get(0);
         int start = intervals.get(0).start;
         int end = intervals.get(0).end;
-        // for (int i = 1; i < occupation.size(); i++) {
-        //     List<Integer> curr = occupation.get(i);
-        //     if (pre.get(1) >= curr.get(0)) {
-        //         pre.set(1, Math.max(pre.get(1), curr.get(1)));
-        //     } else {
-        //         Interval inter = new Interval(pre.get(0), pre.get(1));
-        //         res.add(inter);
-        //         pre = curr;
-        //     }
-        // }
-        // Interval inter = new Interval(pre.get(0), pre.get(1));
-        // res.add(inter);
         for (Interval interval : intervals) {
             if (interval.start <= end) {
                 end = Math.max(end, interval.end);
