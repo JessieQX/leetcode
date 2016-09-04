ad65725e23fe9b3fdca4ba5b32019a2952b6644d
JessieQX <quxuan819@gmail.com>
2016-09-04 09:56:43 -0400

    [56. Merge Intervals][Compile Error]committed by LeetCode Extension

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -11,24 +11,37 @@ public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> res = new ArrayList<>();
         if (intervals == null || intervals.size() == 0) return res;
-        List<List<Integer>> occupation = new ArrayList<>();
+        //List<List<Integer>> occupation = new ArrayList<>();
+        // for (Interval interval : intervals) {
+        //     occupation.add(new ArrayList<>(Arrays.asList(interval.start, interval.end)));
+        // }
+        Collections.sort(intervals, (a, b) -> a.get(0) - b.get(0));
+        //occupation.sort((a, b) -> a.get(0) - b.get(0));
+        //List<Integer> pre = occupation.get(0);
+        int start = intervals.get(0).start;
+        int end = intervals.get(0).end;
+        // for (int i = 1; i < occupation.size(); i++) {
+        //     List<Integer> curr = occupation.get(i);
+        //     if (pre.get(1) >= curr.get(0)) {
+        //         pre.set(1, Math.max(pre.get(1), curr.get(1)));
+        //     } else {
+        //         Interval inter = new Interval(pre.get(0), pre.get(1));
+        //         res.add(inter);
+        //         pre = curr;
+        //     }
+        // }
+        // Interval inter = new Interval(pre.get(0), pre.get(1));
+        // res.add(inter);
         for (Interval interval : intervals) {
-            occupation.add(new ArrayList<>(Arrays.asList(interval.start, interval.end)));
-        }
-        occupation.sort((a, b) -> a.get(0) - b.get(0));
-        List<Integer> pre = occupation.get(0);
-        for (int i = 1; i < occupation.size(); i++) {
-            List<Integer> curr = occupation.get(i);
-            if (pre.get(1) >= curr.get(0)) {
-                pre.set(1, Math.max(pre.get(1), curr.get(1)));
+            if (interval.start <= end) {
+                end = Math.max(end, interval.end);
             } else {
-                Interval inter = new Interval(pre.get(0), pre.get(1));
-                res.add(inter);
-                pre = curr;
+                res.add(new Interval(start, end));
+                start = interval.start;
+                end = interval.end;
             }
         }
-        Interval inter = new Interval(pre.get(0), pre.get(1));
-        res.add(inter);
+        res.add(new Interval(start, end));
         return res;
     }
 }
