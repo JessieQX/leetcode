446f08f73b2ee2e747fdeb787c9f7e711ab7dab6
JessieQX <quxuan819@gmail.com>
2016-09-05 14:25:09 -0400

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -6,9 +6,9 @@ public class Solution {
         int n = p.length();
         boolean[][] f = new boolean[m + 1][n + 1];
         f[0][0] = true;
-        for (int i = 1; i <= m; i++) {
-            f[i][0] = false;
-        }
+        // for (int i = 1; i <= m; i++) {
+        //     f[i][0] = false;
+        // } no need, false is default
         for (int j = 1; j < n; j++) {
             if (p.charAt(j) == '*') {
                 f[0][j + 1] = f[0][j - 1];
@@ -16,10 +16,23 @@ public class Solution {
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (p.charAt(j - 1) != '*') {
-                    f[i][j] = f[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || '.' == p.charAt(j - 1));
-                } else {
-                    f[i][j] = f[i][j - 2] || (s.charAt(i - 1) == p.charAt(j - 2) || '.' == p.charAt(j - 2)) && f[i - 1][j];
+                // if (p.charAt(j - 1) != '*') {
+                //     f[i][j] = f[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || '.' == p.charAt(j - 1));
+                // } else {
+                //     f[i][j] = f[i][j - 2] || (s.charAt(i - 1) == p.charAt(j - 2) || '.' == p.charAt(j - 2)) && f[i - 1][j];
+                // }
+                if (s.charAt(i - 1) == p.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '.') {
+                    f[i][j] = f[i - 1][j - 1];
+                }
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        f[i][j] = f[i][j - 2];
+                    } else {
+                        f[i][j] = f[i - 1][j] || f[i][j - 1] || f[i][j - 2];
+                    }
                 }
             }
         }
