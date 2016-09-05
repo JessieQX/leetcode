73a3bccc5afbcc5b1e355e7baacaed8af199fa39
JessieQX <quxuan819@gmail.com>
2016-09-05 16:22:49 -0400

    [323. Number of Connected Components in an Undirected Graph][Accepted]committed by LeetCode Extension

--- 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
@@ -1,9 +1,10 @@
 public class Solution {
     public int countComponents(int n, int[][] edges) {
         int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = i;
-        }
+        // for (int i = 0; i < n; i++) {
+        //     nums[i] = i;
+        // }
+        Arrays.fill(nums, -1);
         for (int[] edge : edges) {
             int x = find(nums, edge[0]);
             int y = find(nums, edge[1]);
@@ -15,10 +16,12 @@ public class Solution {
         return n;
     }
     private int find(int[] nums, int i) {
-        while (nums[i] != i) {
-            nums[i] = nums[nums[i]];
-            i = nums[i];
-        }
-        return i;
+        // while (nums[i] != i) {
+        //     nums[i] = nums[nums[i]];
+        //     i = nums[i];
+        // }
+        // return i;
+        if (nums[i] == -1) return i;
+        return find(nums, nums[i]);
     }
 }
