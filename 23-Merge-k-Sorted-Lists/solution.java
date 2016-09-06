8344a196e494bacb65a45f7867a3754b91177a25
JessieQX <quxuan819@gmail.com>
2016-09-05 22:59:36 -0400

    [23. Merge k Sorted Lists][Compile Error]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -12,24 +12,53 @@ public class Solution {
         //1->3->4->6
         //7->9
         //[0,1,7]
-        if (lists == null || lists.length == 0) return null;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (a, b) -> (a.val - b.val));
-        for (ListNode node : lists) {
-            while (node != null) {
-                pq.offer(node);
-                node = node.next;
-            }
-        }
+        // if (lists == null || lists.length == 0) return null;
+        // PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (a, b) -> (a.val - b.val));
+        // for (ListNode node : lists) {
+        //     while (node != null) {
+        //         pq.offer(node);
+        //         node = node.next;
+        //     }
+        // }
         
-        ListNode head = pq.poll();
-        ListNode node = head;
-        while (!pq.isEmpty()) {
-            node.next = pq.poll();
-            node = node.next;
-            if (node.next != null) {
-                pq.offer(node.next);
+        // ListNode dummy = new ListNode(0);
+        // ListNode node = dummy;
+        // while (!pq.isEmpty()) {
+        //     node.next = pq.poll();
+        //     node = node.next;
+        //     if (node.next != null) {
+        //         pq.offer(node.next);
+        //     }
+        // }
+        // return head;
+        if (lists==null||lists.size()==0) return null;
+        
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.size(),new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else 
+                    return 1;
             }
+        });
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail=dummy;
+        
+        for (ListNode node:lists)
+            if (node!=null)
+                queue.add(node);
+            
+        while (!queue.isEmpty()){
+            tail.next=queue.poll();
+            tail=tail.next;
+            
+            if (tail.next!=null)
+                queue.add(tail.next);
         }
-        return head;
+        return dummy.next;
     }
 }
