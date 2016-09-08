6247e15ef67f721bca2f68657d5f4ba855f75243
JessieQX <quxuan819@gmail.com>
2016-09-07 22:05:37 -0400

    [212. Word Search II][Compile Error]committed by LeetCode Extension

---
+++ 212-Word-Search-II/solution.java
@@ -0,0 +1,71 @@
+public class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode trie = new TrieNode();
+        for (String word : words) {
+            trie.insert(word);
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs(board, i, j, trie, res, "");
+            }
+        }
+        return res;
+    }
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> res, String str) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+        char c = board[i][j];
+        if (board[i][j] == '#') return;
+        str += board[i][j];
+        if (!node.startsWith(str)) return;
+        if (node.search(str)) {
+            res.add(str);
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j, node, res, str);
+        dfs(board, i + 1, j, node, res, str);
+        dfs(board, i, j - 1, node, res, str);
+        dfs(board, i, j + 1, node, res, str);
+        board[i][j] = c;
+    }
+}
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    String name = "";
+    
+    public void insert(String word) {
+        TrieNode node = this;
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.name = word;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = this;
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return node.name.equals(word);
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = this;
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            if (children[c - 'a'] == null) {
+                return false;
+            }
+            node = node.children[c - 'a'];
+        }
+        return true;
+    }
+}
