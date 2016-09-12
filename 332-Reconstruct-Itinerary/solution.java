8c76dc0bccd94841c4c4594c8c3e0e1db25aebe9
JessieQX <quxuan819@gmail.com>
2016-09-12 17:35:36 -0400

    [332. Reconstruct Itinerary][Accepted]committed by LeetCode Extension

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     private Map<String, PriorityQueue<String>> flights = new HashMap<>();
-    private List<String> res = new ArrayList<>();
+    //private List<String> res = new ArrayList<>();
     public List<String> findItinerary(String[][] tickets) {
         for (String[] s : tickets) {
             flights.putIfAbsent(s[0], new PriorityQueue<>());
             flights.get(s[0]).add(s[1]);
         }
-        //List<String> res = new ArrayList<>();
-        helper("JFK");
+        List<String> res = new ArrayList<>();
+        helper("JFK", res);
         return res;
     }
-    private void helper(String departure) {
+    private void helper(String departure, List<String> res) {
         PriorityQueue<String> queue = flights.get(departure);
-        while (!queue.isEmpty()) {
+        while (queue != null && !queue.isEmpty()) {
             String destination = queue.poll();
-            helper(destination);
+            helper(destination, res);
         }
         res.add(0, departure);
     }
