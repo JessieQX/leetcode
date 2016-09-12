af850d9f8fc73ead18e95d7f3b334a0605bf743d
JessieQX <quxuan819@gmail.com>
2016-09-12 17:35:02 -0400

    [332. Reconstruct Itinerary][Runtime Error]committed by LeetCode Extension

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     private Map<String, PriorityQueue<String>> flights = new HashMap<>();
-    //private List<String> res = new ArrayList<>();
+    private List<String> res = new ArrayList<>();
     public List<String> findItinerary(String[][] tickets) {
         for (String[] s : tickets) {
             flights.putIfAbsent(s[0], new PriorityQueue<>());
             flights.get(s[0]).add(s[1]);
         }
-        List<String> res = new ArrayList<>();
-        helper("JFK", res);
+        //List<String> res = new ArrayList<>();
+        helper("JFK");
         return res;
     }
-    private void helper(String departure, List<String> res) {
+    private void helper(String departure) {
         PriorityQueue<String> queue = flights.get(departure);
         while (!queue.isEmpty()) {
             String destination = queue.poll();
-            helper(destination, res);
+            helper(destination);
         }
         res.add(0, departure);
     }
