a73d1016d81e5a6c69f9e66f0d2b1f67e4a10035
JessieQX <quxuan819@gmail.com>
2016-09-14 18:26:32 -0400

    [363. Max Sum of Rectangle No Larger Than K][Compile Error]committed by LeetCode Extension

---
+++ 363-Max-Sum-of-Rectangle-No-Larger-Than-K/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int res = Integer.MIN_VALUE;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        for (int left = 0; left < n; left++) {
+            int[] sums = new int[m];
+            for (int right = left; right < n; right++) {
+                for (int i = 0; i < m; i++) {
+                    sums[i] += matrix[i][right];
+                }
+                Set<Integer> set = new TreeSet<>();
+                set.add(0);
+                int currSum = 0;
+                for (int sum : sums) {
+                    currSum += sum;
+                    Integer num = set.ceiling(currSum - k);
+                    if (num != null) res = Math.max(res, currSum - num);
+                    set.add(currSum);
+                }
+            }
+        }
+        return res;
+    }
+}
