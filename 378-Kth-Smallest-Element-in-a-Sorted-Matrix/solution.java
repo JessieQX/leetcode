f8339953f9dcdd821fa71ddee8bfae74692aa171
JessieQX <quxuan819@gmail.com>
2016-09-15 19:44:08 -0400

    [378. Kth Smallest Element in a Sorted Matrix][Compile Error]committed by LeetCode Extension

---
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            pq.offer(new Point(0, i, matrix[0][i]));
+        }
+        for (int i = 0; i < k - 1; i++) {
+            Point p = pq.poll();
+            if (p.x == n - 1) continue;
+            pq.offer(new Point(p.x + 1, p.y, matrix[p.x + 1][p.y]));
+        }
+        return pq.poll();
+    }
+    class Point {
+        int x, int y, int val;
+        public Point(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+        // public int compareTo(Point p) {
+        //     return this.val - p.val;
+        // }
+    }
+}
