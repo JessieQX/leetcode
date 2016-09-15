f0f2ec92f8332ca4024a6b23164683a45c22e943
JessieQX <quxuan819@gmail.com>
2016-09-15 19:46:28 -0400

    [378. Kth Smallest Element in a Sorted Matrix][Runtime Error]committed by LeetCode Extension

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -12,15 +12,13 @@ public class Solution {
         }
         return pq.poll().val;
     }
-    class Point implements Comparable<Point> {
+    class Point {
         int x, y, val;
         public Point(int x, int y, int val) {
             this.x = x;
             this.y = y;
             this.val = val;
         }
-        public int compareTo(Point p) {
-            return this.val - p.val;
-        }
+        
     }
 }
