a3257e4f78ff8ac4df8d672a2bf9f5b202c2fa93
JessieQX <quxuan819@gmail.com>
2016-09-15 22:16:12 -0400

    [373. Find K Pairs with Smallest Sums][Compile Error]committed by LeetCode Extension

---
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<int[]> res = new ArrayList<>();
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                pq.offer(i, j, nums[i] + nums[j]);
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            Point p = pq.poll();
+            int x = p.x;
+            int y = p.y;
+            res.add(new int[]{x, y});
+        }
+        return res;
+    }
+    class Point implements Comparable<Point> {
+        int x, y, val;
+        public Point (int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+        public int compareTo(Point p) {
+            this.val - p.val;
+        }
+    }
+}
