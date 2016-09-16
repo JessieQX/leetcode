dc359391960c96154bfadc0c9ae239dfb62462a9
JessieQX <quxuan819@gmail.com>
2016-09-15 21:15:31 -0400

    [394. Decode String][Compile Error]committed by LeetCode Extension

---
+++ 394-Decode-String/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> wordStack = new Stack<>();
+        int i = 0;
+        String res = "";
+        while (i < s.length()) {
+            //char c = s.charAt(i);
+            if (Character.isDigit(s.charAt(i))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(i))) {
+                    count = count * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                countStack.push(count);
+            }
+            else if (c == '[') {
+                wordStack.push(res);
+                res = "";
+                i++;
+            }
+            else if (c == ']') {
+                StringBuilder sb = new StringBuilder(wordStack.pop());
+                int num = countStack.pop();
+                for (int i = 0; i < num; i++) {
+                    sb.append(res);
+                }
+                res = sb.toString();
+                i++;
+            }
+            else {
+                res += s.charAt(i);
+            }
+        }
+    }
+}
