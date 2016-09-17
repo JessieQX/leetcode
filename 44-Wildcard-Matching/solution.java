0531f438672a875cfc6839fc298e665fec0a2640
JessieQX <quxuan819@gmail.com>
2016-09-17 18:37:43 -0400

    [44. Wildcard Matching][Runtime Error]committed by LeetCode Extension

---
+++ 44-Wildcard-Matching/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s.isEmpty() || p.isEmpty()) return false;
+        int m = s.length();
+        int n = p.length();
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                f[0][j] = true;
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    f[i][j] = f[i - 1][j - 1];
+                } else {
+                    if (p.charAt(i - 1) == '*') {
+                        f[i][j] = f[i - 1][j - 1] || f[i - 1][j] || f[i][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+}
