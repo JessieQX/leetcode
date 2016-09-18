478fbcf47c7b8ae006759be5669531615f23c26d
JessieQX <quxuan819@gmail.com>
2016-09-18 18:17:25 -0400

    [282. Expression Add Operators][Accepted]committed by LeetCode Extension

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,23 +1,26 @@
 public class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<>();
-        if (num == null || num.length == 0) return res;
+        if (num == null || num.length() == 0) return res;
         helper(res, "", num, target, 0, 0, 0);
         return res;
     }
     private void helper(List<String> res, String path, String num, int target, int start, long val, long multed) {
-        if (val == target) {
-            res.add(path);
-            return;
+        if (start == num.length()) {
+            if (val == target) {
+                res.add(path);
+                return;
+            }
         }
         for (int i = start; i < num.length(); i++) {
+            if (i != start && num.charAt(start) == '0') break;
             long curr = Long.parseLong(num.substring(start, i + 1));
             if (start == 0) {
                 helper(res, path + curr, num, target, i + 1, curr, curr);
             } else {
-                helper(res, path + "+" + curr, num, target, i + 1, val + curr, val + curr);
-                helper(res, path + "-" + curr, num, target, i + 1, val - curr, val - curr);
-                helper(res, path + "*" + curr, num, target, i + 1, val * curr, val * curr);
+                helper(res, path + "+" + curr, num, target, i + 1, val + curr, curr);
+                helper(res, path + "-" + curr, num, target, i + 1, val - curr, -curr);
+                helper(res, path + "*" + curr, num, target, i + 1, val - multed + multed * curr, multed * curr);
             }
         }
     }
