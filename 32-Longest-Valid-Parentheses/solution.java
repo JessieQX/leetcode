e40090e253e371bff081fed30c56414b4d4178d6
JessieQX <quxuan819@gmail.com>
2016-09-18 11:19:02 -0400

    [32. Longest Valid Parentheses][Runtime Error]committed by LeetCode Extension

--- 32-Longest-Valid-Parentheses/solution.java
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -1,40 +1,22 @@
 public class Solution {
     public int longestValidParentheses(String s) {
-        //if (s == null || s.length() == 0) return 0;
-        // Stack<Integer> stack = new Stack<>();
-        // int index = -1;
-        // int max = 0;
-        // for (int i = 0; i < s.length(); i++) {
-        //     if (s.charAt(i) == '(') {
-        //         stack.push(i);
-        //     } else {
-        //         if (stack.isEmpty()) {
-        //             index = i;
-        //         } else {
-        //             stack.pop();
-        //             if (stack.isEmpty()) {
-        //                 max = Math.max(max, i - index);
-        //             } else {
-        //                 max = Math.max(max, i - stack.peek());
-        //             }
-        //         }
-        //     }
-        // }
-        // return max;
-        Stack<Integer> stack = new Stack<Integer>();
-        int max=0;
-        int left = -1;
-        for(int j=0;j<s.length();j++){
-            if(s.charAt(j)=='(') stack.push(j);            
-            else {
-                if (stack.isEmpty()) left=j;
-                else{
-                    stack.pop();
-                    if(stack.isEmpty()) max=Math.max(max,j-left);
-                    else max=Math.max(max,j-stack.peek());
+        if (s == null || s.length() == 0) return 0;
+        int n = s.length();
+        int[] f = new int[n];
+        int res = 0;
+        int left = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(') left++;
+            else if (c == ')' && left > 0) {
+                f[i] = f[i - 1] + 2;
+                if (i - f[i] > 0) {
+                    f[i] += f[i - f[i]];
                 }
+                left--;
             }
+            res = Math.max(res, f[i]);
         }
-        return max;
+        return f[n];
     }
 }
