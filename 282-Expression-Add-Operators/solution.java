0a3757af692f623ffb4f6081fbb69cc1d7b2a42c
JessieQX <quxuan819@gmail.com>
2016-09-18 18:06:24 -0400

    [282. Expression Add Operators][Compile Error]committed by LeetCode Extension

---
+++ 282-Expression-Add-Operators/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        if (num == null || num.length == 0) return res;
+        helper(res, "", num, target, 0, 0, 0);
+        return res;
+    }
+    private void helper(List<String> res, String path, String num, int target, int start, long val, long multed) {
+        if (val == target) {
+            res.add(path);
+            return;
+        }
+        for (int i = start; i < num.length(); i++) {
+            long curr = Long.parseLong(num.substring(start, i + 1));
+            if (start == 0) {
+                helper(res, path + curr, num, target, i + 1, curr, curr);
+            } else {
+                helper(res, path + "+" + curr, num, target, i + 1, val + curr, val + curr);
+                helper(res, path + "-" + curr, num, target, i + 1, val - curr, val - curr);
+                helper(res, path + "*" + curr, num, target, i + 1, val * curr, val * curr);
+            }
+        }
+    }
+}
