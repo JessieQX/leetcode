e6206d666f9b010b375e405de5e06ed94a6c1256
JessieQX <quxuan819@gmail.com>
2016-09-17 21:05:28 -0400

    [44. Wildcard Matching][Compile Error]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,5 +1,7 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
+        if (s.isEmpty() && p.isEmpty()) return true;
+        if ((s.isEmpty() || p.isEmpty()) && (s.equals("*") || p.equals("*")) return true
         if (s.isEmpty() || p.isEmpty()) return false;
         int m = s.length();
         int n = p.length();
@@ -7,18 +9,19 @@ public class Solution {
         f[0][0] = true;
         for (int j = 1; j <= n; j++) {
             if (p.charAt(j - 1) == '*') {
-                f[0][j] = true;
+                f[0][j] = f[0][j - 1];
             }
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
                     f[i][j] = f[i - 1][j - 1];
-                } else {
-                    if (p.charAt(i - 1) == '*') {
-                        f[i][j] = f[i - 1][j] || f[i][j - 1];
-                    }
+                } else if (p.charAt(j - 1) == '*') {
+                    
+                        f[i][j] = f[i - 1][j - 1] || f[i - 1][j] || f[i][j - 1];
+                    
                 }
+                
             }
         }
         return f[m][n];
