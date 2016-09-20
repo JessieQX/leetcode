a89f7dc688eff272b303cbbc22e7ee97dc30f4dc
JessieQX <quxuan819@gmail.com>
2016-09-19 22:08:09 -0400

    [45. Jump Game II][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 45-Jump-Game-II/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int jump(int[] nums) {
+        int[] steps = new int[nums.length];
+        steps[0] = 0;
+        for (int i = 1; i < nums.length; i++) {
+            steps[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < i; j++) {
+                if (steps[j] != Integer.MAX_VALUE && nums[j] + j >= i) {
+                    // steps[i] = steps[j] + 1;
+                    // break;
+                    steps[i] = Math.min(steps[j] + 1, steps[i]);
+                }
+            }
+        }
+        return steps[nums.length - 1];
+    }
+}
