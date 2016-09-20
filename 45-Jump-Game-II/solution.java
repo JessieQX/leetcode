394771585b6274ef9b7ae3096049ff792b94b55c
JessieQX <quxuan819@gmail.com>
2016-09-19 22:23:14 -0400

    [45. Jump Game II][Compile Error]committed by LeetCode Extension

--- 45-Jump-Game-II/solution.java
+++ 45-Jump-Game-II/solution.java
@@ -1,17 +1,28 @@
 public class Solution {
     public int jump(int[] nums) {
-        int[] steps = new int[nums.length];
-        steps[0] = 0;
-        for (int i = 1; i < nums.length; i++) {
-            steps[i] = Integer.MAX_VALUE;
-            for (int j = 0; j < i; j++) {
-                if (steps[j] != Integer.MAX_VALUE && nums[j] + j >= i) {
-                    steps[i] = steps[j] + 1;
-                    break;
-                    //steps[i] = Math.min(steps[j] + 1, steps[i]);
-                }
-            }
+        // int[] steps = new int[nums.length];
+        // steps[0] = 0;
+        // for (int i = 1; i < nums.length; i++) {
+        //     steps[i] = Integer.MAX_VALUE;
+        //     for (int j = 0; j < i; j++) {
+        //         if (steps[j] != Integer.MAX_VALUE && nums[j] + j >= i) {
+        //             steps[i] = steps[j] + 1;
+        //             break;
+        //             //steps[i] = Math.min(steps[j] + 1, steps[i]);
+        //         }
+        //     }
+        // }
+        // return steps[nums.length - 1];
+        int step_count = 0;
+        int last_jump_max = 0;
+        int current_jump_max = 0;
+        for(int i=0; i<A.length-1; i++) {
+            current_jump_max = Math.max(current_jump_max, i+A[i]);
+            if( i == last_jump_max ) {
+                step_count++;
+                last_jump_max = current_jump_max;
+            } 
         }
-        return steps[nums.length - 1];
+        return step_count;
     }
 }
