98fbf16f9be275f5bc02862bbcb23e8ba93fd270
JessieQX <quxuan819@gmail.com>
2016-09-19 22:23:56 -0400

    [45. Jump Game II][Accepted]committed by LeetCode Extension

--- 45-Jump-Game-II/solution.java
+++ 45-Jump-Game-II/solution.java
@@ -16,8 +16,8 @@ public class Solution {
         int step_count = 0;
         int last_jump_max = 0;
         int current_jump_max = 0;
-        for(int i=0; i<A.length-1; i++) {
-            current_jump_max = Math.max(current_jump_max, i+A[i]);
+        for(int i=0; i<nums.length-1; i++) {
+            current_jump_max = Math.max(current_jump_max, i+nums[i]);
             if( i == last_jump_max ) {
                 step_count++;
                 last_jump_max = current_jump_max;
