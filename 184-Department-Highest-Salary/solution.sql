52f7875e6933707407650e21d4294258feb6279e
JessieQX <quxuan819@gmail.com>
2016-10-03 23:40:59 -0400

    [184. Department Highest Salary][Runtime Error]committed by LeetCode Extension

--- 184-Department-Highest-Salary/solution.sql
+++ 184-Department-Highest-Salary/solution.sql
@@ -1,15 +1,7 @@
 # Write your MySQL query statement below
-select Department.Name as Department, Employee.Name as Employee, Employee.Salary 
-from Department, Employee, (select Employee.DepartmentId, max(Employee.Salary) as max from Employee group by Employee.DepartmentId) New
-where Employee.DepartmentId = Department.Id
-AND New.max = Employee.Salary
-and New.DepartmentId = Department.Id;
 
-select Department.name as Department, NEW.Name as Employee, NEW.Salary as Salary
-from Department, (select Employee.DepartmentId as NEWID, Employee.Name as Name, max(Employee.Salary) as Salary from Employee group by Employee.DepartmentId) NEW
-where Department.Id = NEW.NEWID;
 
-select New.name as Department, New.Name as Employee, max(New.Salary) as Salary
-from (select Employee.Id, Employee.Name as Name, Employee.Salary as Salary, Department.Name from Employee, Department
+select New.Name as Department, New.Name as Employee, max(New.Salary) as Salary
+from (select Employee.Id, Employee.Name as Name, Employee.Salary as Salary, Department.Name as Name from Employee, Department
     where Employee.DepartmentId = Department.Id) New
 group by New.Salary;
