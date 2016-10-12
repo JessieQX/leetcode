3d9bd7c213ef111bfebbac350a079b9aec7fd6b5
JessieQX <quxuan819@gmail.com>
2016-10-12 16:52:44 -0400

    [330. Patching Array][Accepted]committed by LeetCode Extension

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-        int edge = 1, count = 0, i = 0;
+        long edge = 1;
+        int count = 0, i = 0;
         while (edge <= n) {
             if (i < nums.length && nums[i] <= edge) {
                 edge += nums[i++];
